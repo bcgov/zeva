1e53bc5978bbb2d2a6cf77ad8bec07d67db52174
Robert Johnstone <rob@plasticviking.com>
2019-11-28 12:38:26 -0800

    python backend

--- services/python-transaction/zeva/service.py
+++ services/python-transaction/zeva/service.py
@@ -1,16 +1,35 @@
+import importlib
+import logging
+
+import watchdog
+
 from concurrent import futures
 
 import grpc
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 from generated import transactions_pb2_grpc
-from services.transaction_service import TransactionServicer
+import services
+import services.transaction_service
+
+class ReloadHandler(FileSystemEventHandler):
+
+    def on_any_event(self, event):
+        logging.info('Handling filesystem event')
+        importlib.reload(services)
+        importlib.reload(services.transaction_service)
+
+
+def database_wait(wait=10000):
+    logging.info('checking database connectivity')
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
 
     transactions_pb2_grpc.add_TransactionListServicer_to_server(
-        TransactionServicer(),
+        services.transaction_service.TransactionServicer(),
         server
     )
 
@@ -19,4 +38,19 @@ def serve():
     server.wait_for_termination()
 
 
-serve()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    event_handler = ReloadHandler()
+    observer = Observer()
+    observer.schedule(event_handler, '.', recursive=True)
+    observer.start()
+    try:
+        database_wait()
+        serve()
+    except Exception as e:
+        logging.error('error')
+
+    observer.join()
