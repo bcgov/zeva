3b5ddbbc01654ff9477df03995f7b3a0cd9b9483
Richard Tan <richard.tan@gov.bc.ca>
2019-12-16 15:56:47 -0800

    Header and navigation

--- frontend/src/app/PageLayout.js
+++ frontend/src/app/PageLayout.js
@@ -3,17 +3,27 @@ import { hot } from 'react-hot-loader/root';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+import Navbar from './components/Navbar';
+
+library.add(fab, far, fas);
+
 const PageLayout = (props) => {
   const { children, keycloak } = props;
 
   return (
     <div>
+      <Navbar />
       {keycloak.authenticated && (
         <div>
           <strong>Authenticated</strong>
           <ul>
             {keycloak.realmAccess.roles.map((role) => (
-              <li>{role}</li>
+              <li key={role}>{role}</li>
             ))}
           </ul>
         </div>
---
+++ frontend/src/app/components/Navbar.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class Navbar extends Component {
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div id="navbar">
+        <div className="row header">
+          <div className="col-sm-9">
+            <div className="brand-logo" />
+
+            <h1>Zero Emission Vehicle Credit System</h1>
+          </div>
+
+          <div className="col-sm-3">
+            <div className="logged-in-info">
+              <div>
+                <h5>TFRS Fantasic Fuels</h5>
+                <span>Credit Balance: 123,456</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row sub-header">
+          <div className="col-sm-9 links">
+            <NavLink
+              activeClassName="active"
+              to="/"
+            >
+              Home
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              to="/compliance-reports"
+            >
+              Compliance Reports
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              to="/initiative-agreements"
+            >
+              Initiative Agreements
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              to="/credit-transactions"
+            >
+              Credit Transactions
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              to="/sales"
+            >
+              Sales
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              to="/zev-models"
+            >
+              ZEV Models
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              to="/organization-details"
+            >
+              Organization Details
+            </NavLink>
+          </div>
+
+          <div className="col-sm-3 user-control-panel">
+            <div>
+              Richard Tan
+            </div>
+            <NavLink
+              activeClassName="active"
+              className="notifications"
+              to="/notifications"
+            >
+              <FontAwesomeIcon icon="bell" />
+            </NavLink>
+            <NavLink
+              activeClassName="active"
+              className="help"
+              to="/help"
+            >
+              <FontAwesomeIcon icon={['far', 'question-circle']} />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Navbar.defaultProps = {
+};
+
+Navbar.propTypes = {
+};
+
+export default Navbar;
