25326a192a18678572a4369cf989e4147bc0977f
Rob Johnstone <rob@plasticviking.com>
2019-12-17 13:09:50 +0800

    Add Organization GRPC Server and Client

--- services/python-transaction/generate.sh
+++ services/python-transaction/generate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 -m grpc_tools.protoc -I../../protos --python_out=./src/generated --grpc_python_out=./src/generated ../../protos/transactions.proto
+python3 -m grpc_tools.protoc -I../../protos --python_out=./src/generated --grpc_python_out=./src/generated ../../protos/*proto
