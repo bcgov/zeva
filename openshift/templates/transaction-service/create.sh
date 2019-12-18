4c7d230e37971739d513bc23de86df051c5b2ca0
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2019-12-18 10:17:49 -0800

    Add new Openshift Templates (#14)

--- openshift/templates/transaction-service/create.sh
+++
@@ -1,2 +0,0 @@
-oc process -f ./transaction-service-bc.yaml | oc create -f - -n tbiwaq-tools --dry-run=true
-oc process -f ./transaction-service-dc.yaml CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=1100M MEMORY_LIMIT=2G ENV_NAME=dev | oc create -f - -n tbiwaq-dev --dry-run=true
