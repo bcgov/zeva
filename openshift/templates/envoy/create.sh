0abca0877c4ea4c578aec9aed5e34bf82fa485e9
Kuan Fan <kuan.fan@gov.bc.ca>
2019-12-18 10:17:06 -0800

    add create.sh

--- openshift/templates/envoy/create.sh
+++ openshift/templates/envoy/create.sh
@@ -1,1 +1,2 @@
 oc process -f ./envoy-dc.yaml CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=1100M MEMORY_LIMIT=2G ENV_NAME=dev | oc create -f - -n tbiwaq-dev --dry-run=true
+oc process -f ./envoy-bc.yaml | oc create -f - -n tbiwaq-tools --dry-run=true
