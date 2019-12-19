0b266e1630589874c94571f624282bd274c08994
Kuan Fan <kuan.fan@gov.bc.ca>
2019-12-19 10:58:57 -0800

    update openshift templates

--- openshift/templates/python-backend/create.sh
+++ openshift/templates/python-backend/create.sh
@@ -1,1 +1,2 @@
 oc process -f ./python-backend-dc.yaml CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=1100M MEMORY_LIMIT=2G ENV_NAME=dev DASH_ENV_NAME=-dev POSTGRESQL_USER=???? POSTGRESQL_PASSWORD=???? | oc create -f - -n tbiwaq-dev --dry-run=true
+oc process -f ./python-backend-bc.yaml | oc create -f - -n tbiwaq-tools --dry-run=true
