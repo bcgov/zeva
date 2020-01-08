4dcb3ae388cd7f87c43f7aa25ac1ae29f7afde78
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2020-01-08 13:42:16 -0800

    Initial bcdk setup (#18)
    Setup .jenkins and .pipeline

---
+++ .pipeline/lib/deploy.js
@@ -0,0 +1,66 @@
+"use strict";
+const { OpenShiftClientX } = require("@bcgov/pipeline-cli");
+const path = require("path");
+
+module.exports = settings => {
+  const phases = settings.phases;
+  const options = settings.options;
+  const phase = options.env;
+  const changeId = phases[phase].changeId;
+  const oc = new OpenShiftClientX(Object.assign({ namespace: phases[phase].namespace }, options));
+
+  const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, "../../openshift"));
+  var objects = [];
+
+  // The deployment of your cool app goes here ▼▼▼
+  //deploy envoy
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/envoy/envoy-dc.yaml`, {
+    'param':{
+      'NAME': phases[phase].name,
+      'SUFFIX': phases[phase].suffix,
+      'VERSION': phases[phase].tag,
+      'ENV_NAME': phases[phase].phase,
+      'CPU_REQUEST': '100m',
+      'CPU_LIMIT': '300m',
+      'MEMORY_REQUEST': '200m',
+      'MEMORY_LIMIT': '500M'
+    }
+  }))
+  // deploy frontend
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-dc.yaml`, {
+    'param':{
+      'NAME': phases[phase].name,
+      'SUFFIX': phases[phase].suffix,
+      'VERSION': phases[phase].tag,
+      'ENV_NAME': phases[phase].phase,
+      'DASH_ENV_NAME': phases[phase].ssoSuffix,
+      'CPU_REQUEST': '100m',
+      'CPU_LIMIT': '500m',
+      'MEMORY_REQUEST': '1100M',
+      'MEMORY_LIMIT': '2G'
+    }
+  }))
+  // deploy database
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/postgresql/postgresql-dc.yaml`, {
+    'param':{
+      'NAME': phases[phase].name,
+      'SUFFIX': phases[phase].suffix,
+      'VERSION': phases[phase].tag,
+      'ENV_NAME': phases[phase].phase,
+      'CPU_REQUEST': '100m',
+      'CPU_LIMIT': '500m',
+      'MEMORY_REQUEST': '200M',
+      'MEMORY_LIMIT': '500M'
+    }
+  }))
+
+  oc.applyRecommendedLabels(
+    objects,
+    phases[phase].name,
+    phase,
+    `${changeId}`,
+    phases[phase].instance,
+  );
+  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag);
+  oc.applyAndDeploy(objects, phases[phase].instance);
+};
