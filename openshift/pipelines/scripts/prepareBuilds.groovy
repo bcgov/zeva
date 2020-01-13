980e23e1abd5122df228abf05b4f3937fba2ab35
Kuan Fan <kuan.fan@gov.bc.ca>
2020-01-13 14:13:40 -0800

    remove echo

--- openshift/pipelines/scripts/prepareBuilds.groovy
+++ openshift/pipelines/scripts/prepareBuilds.groovy
@@ -1,8 +1,10 @@
 def buildStages(String envName, String zevaRelease) {
+    print ("in buildStages")
     def buildList = []
     def buildStages = [:]
     buildStages.put('Build Frontend', prepareBuildFrontend(envName, zevaRelease))
     buildList.add(buildStages)
+    print ("out buildStages")
     return buildList
 }
 
@@ -11,11 +13,13 @@ def prepareBuildFrontend(String envName, String zevaRelease) {
         stage('Build-Frontend') {
             timeout(30) {
                 script {
+                    print ("in prepareBuildFrontend")
                     openshift.withProject("tbiwaq-tools") {
                         def frontendJson = openshift.process(readFile(file:'openshift/templates/frontend/frontend-bc-release.json'), "-p", "RELEASE=${zevaRelease}", 'GIT_URL=$https://github.com/bcgov/zeva.git', "GIT_REF=${RELEASE}")
                         openshift.apply(frontendJson)
                         def frontendBuildSelector = openshift.selector("bc", "frontend")
                         frontendBuildSelector.startBuild("--wait")
+                        print ("out prepareBuildFrontend")
                     }
                 } //end of script
             } //end of timeout
