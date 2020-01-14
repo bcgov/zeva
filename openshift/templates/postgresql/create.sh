33e52b14494f9c0854f1886fee58015f993d83df
Kuan Fan <kuan.fan@gov.bc.ca>
2020-01-13 16:52:28 -0800

    update db image to user new storage

--- openshift/templates/postgresql/create.sh
+++ openshift/templates/postgresql/create.sh
@@ -1,4 +1,2 @@
-oc process -f ./postgresql-dc.yaml CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=1100M MEMORY_LIMIT=2G | oc create -f - -n tbiwaq-dev --dry-run=true
-
-
-oc process -f ./postgresql-dc.yaml ENV_NAME=dev CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=256Mi MEMORY_LIMIT=1Gi | oc create -f - -n tbiwaq-dev --dry-run=true
+oc process -f ./postgresql-release-dc.yaml CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=1100M MEMORY_LIMIT=2G | oc create -f - -n tbiwaq-dev --dry-run=true
+oc process -f ./postgresql-release-dc.yaml ENV_NAME=dev CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=256Mi MEMORY_LIMIT=2Gi | oc create -f - -n tbiwaq-dev --dry-run=true
