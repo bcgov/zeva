0ff2e3df85de67cd40800f202571bfca2b102954
Kuan Fan <kuan.fan@gov.bc.ca>
2020-01-23 12:07:14 -0800

    update release pipeline

--- openshift/pipelines/scripts/prepareBuilds.groovy
+++ openshift/pipelines/scripts/prepareBuilds.groovy
@@ -12,6 +12,23 @@ def prepareBuildFrontend(String envName, String zevaRelease) {
             timeout(30) {
                 script {
                     openshift.withProject("tbiwaq-tools") {
+                        def frontendyaml = openshift.process(readFile(file:'openshift/templates/frontend/frontend-bc-release.yaml'), '-p', "ZEVA_RELEASE=${zevaRelease}")
+                        openshift.apply(frontendyaml)
+                        def frontendBuildSelector = openshift.selector("bc", "frontend")
+                        frontendBuildSelector.startBuild("--wait")
+                    }
+                } //end of script
+            } //end of timeout
+        }
+    }
+}
+
+def prepareBuildBackend(String envName, String zevaRelease) {
+    return {
+        stage('Build-Frontend') {
+            timeout(30) {
+                script {
+                    openshift.withProject("tbiwaq-tools") {
                         def frontendyaml = openshift.process(readFile(file:'openshift/templates/frontend/frontend-bc-release.yaml'), '-p', 'GIT_URL=https://github.com/bcgov/zeva.git', "GIT_REF=${zevaRelease}")
                         openshift.apply(frontendyaml)
                         def frontendBuildSelector = openshift.selector("bc", "frontend")
--- openshift/templates/config/create.sh
+++ openshift/templates/config/create.sh
@@ -1,1 +1,2 @@
 oc process -f configmap.yaml ENV_NAME=dev SSO_NAME=sso-dev KEYCLOAK_REALM=rzh2zkjq | oc apply -f - -n tbiwaq-dev --dry-run=true
+oc process -f secrets.yaml | oc apply -f - -n tbiwaq-dev --dry-run=true
