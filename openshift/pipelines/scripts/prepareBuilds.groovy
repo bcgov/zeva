2495e342e7a9233f6fc99bb2d40850b2e79f9735
Kuan Fan <kuan.fan@gov.bc.ca>
2020-01-23 12:30:14 -0800

    update release pipeline

--- openshift/pipelines/scripts/prepareBuilds.groovy
+++ openshift/pipelines/scripts/prepareBuilds.groovy
@@ -2,6 +2,7 @@ def buildStages(String envName, String zevaRelease) {
     def buildList = []
     def buildStages = [:]
     buildStages.put('Build Frontend', prepareBuildFrontend(envName, zevaRelease))
+    buildStages.put('Build Backend', prepareBuildBackend(envName, zevaRelease))
     buildList.add(buildStages)
     return buildList
 }
@@ -25,14 +26,14 @@ def prepareBuildFrontend(String envName, String zevaRelease) {
 
 def prepareBuildBackend(String envName, String zevaRelease) {
     return {
-        stage('Build-Frontend') {
+        stage('Build-Backend') {
             timeout(30) {
                 script {
                     openshift.withProject("tbiwaq-tools") {
-                        def frontendyaml = openshift.process(readFile(file:'openshift/templates/frontend/frontend-bc-release.yaml'), '-p', 'GIT_URL=https://github.com/bcgov/zeva.git', "GIT_REF=${zevaRelease}")
-                        openshift.apply(frontendyaml)
-                        def frontendBuildSelector = openshift.selector("bc", "frontend")
-                        frontendBuildSelector.startBuild("--wait")
+                        def backendyaml = openshift.process(readFile(file:'openshift/templates/backend/backend-bc-release.yaml'), '-p', "GIT_REF=${zevaRelease}")
+                        openshift.apply(backendyaml)
+                        def backendBuildSelector = openshift.selector("bc", "backend")
+                        backendBuildSelector.startBuild("--wait")
                     }
                 } //end of script
             } //end of timeout
