fb525ec43b7f9d08311d5641b0f329f68a525c8d
Richard Tan <richard.tan@gov.bc.ca>
2020-01-23 16:44:13 -0800

    Added vehicle fuel types and some hybrid vehicles

---
+++ backend/api/fixtures/operational/0003_add_vehicle_fuel_types.py
@@ -0,0 +1,37 @@
+from django.db import transaction
+
+from api.management.data_script import OperationalDataScript
+from api.models.vehicle_type import FuelType
+
+
+class AddFuelTypes(OperationalDataScript):
+    """
+    Adds the Fuel types found in the NRCAN 2019 Fuel Consumption Guide
+    """
+    is_revertable = False
+    comment = 'Adds the Fuel types found in the NRCAN 2019 Fuel Consumption ' \
+              'Guide'
+
+    def check_run_preconditions(self):
+        return True
+
+    @transaction.atomic
+    def run(self):
+        FuelType.objects.create(
+            code="B",
+            description="Electricity",
+            effective_date="2019-01-01"
+        )
+        FuelType.objects.create(
+            code="BX",
+            description="Electricity/Regular Gasoline",
+            effective_date="2019-01-01"
+        )
+        FuelType.objects.create(
+            code="BZ",
+            description="Electricity/Premium Gasoline",
+            effective_date="2019-01-01"
+        )
+
+
+script_class = AddFuelTypes
