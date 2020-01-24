d03441f28e95a6def615943aa8216c9f9696ae91
Kuan Fan <kuan.fan@gov.bc.ca>
2020-01-24 15:41:11 -0800

    update templates

--- openshift/templates/config/create.sh
+++ openshift/templates/config/create.sh
@@ -1,2 +1,7 @@
 oc process -f configmap.yaml ENV_NAME=dev SSO_NAME=sso-dev KEYCLOAK_REALM=rzh2zkjq | oc apply -f - -n tbiwaq-dev --dry-run=true
-oc process -f secrets.yaml | oc apply -f - -n tbiwaq-dev --dry-run=true
+
+## do not use apply otherwise secrets could be overrided when rerun the command
+oc process -f secrets.yaml | oc create -f - -n tbiwaq-dev --dry-run=true
+
+## apply nfs storage through Openshift service catalog
+
