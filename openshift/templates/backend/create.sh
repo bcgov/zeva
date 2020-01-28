c29eb4228059a0a9e1c8a94eeb039885326b56ff
Kuan Fan <kuan.fan@gov.bc.ca>
2020-01-28 11:56:31 -0800

    update

--- openshift/templates/backend/create.sh
+++ openshift/templates/backend/create.sh
@@ -1,3 +1,9 @@
-oc process -f ./backend-bc-release.yaml GIT_URL=https://github.com/bcgov/zeva.git GIT_REF=pipeline-release2 | oc apply -f - -n tbiwaq-tools --dry-run=true
-oc tag tbiwaq-tools/backend:pipeline-release2 tbiwaq-dev/backend:dev
+
+### build backend
+oc process -f ./backend-bc-release.yaml ZEVA_RELEASE=pipeline-release3 | oc apply -f - -n tbiwaq-tools --dry-run=true
+
+### tag dev image
+oc tag tbiwaq-tools/backend:pipeline-release3 tbiwaq-dev/backend:dev
+
+### deploy dev image
 oc process -f ./backend-dc-release.yaml ENV_NAME=dev CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=512Mi MEMORY_LIMIT=2Gi | oc apply -f - -n tbiwaq-dev --dry-run=true
--- openshift/templates/config/create.sh
+++ openshift/templates/config/create.sh
@@ -1,7 +1,15 @@
+## create configmap
 oc process -f configmap.yaml ENV_NAME=dev SSO_NAME=sso-dev KEYCLOAK_REALM=rzh2zkjq | oc apply -f - -n tbiwaq-dev --dry-run=true
 
-## do not use apply otherwise secrets could be overrided when rerun the command
+## Create secretes for rabbitmq
+
+## create secrets for keycloak
+## find the value for KEYCLOAK_SA_CLIENT_SECRET from keycloak consol
+oc process -f keycloak-secret.yaml KEYCLOAK_SA_CLIENT_SECRET=*** | oc create -f - -n tbiwaq-dev --dry-run=true
+
+## do not use oc apply otherwise secrets could be overrided when rerun the command
 oc process -f secrets.yaml | oc create -f - -n tbiwaq-dev --dry-run=true
+
 
 ## apply nfs storage through Openshift service catalog
 
