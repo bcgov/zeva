241d124b5929cc2080e62ee8160788c90007f851
Kuan Fan <kuan.fan@gov.bc.ca>
2020-01-31 14:29:44 -0800

    update pipeline

--- openshift/pipelines/scripts/prepareDeployments.groovy
+++ openshift/pipelines/scripts/prepareDeployments.groovy
@@ -1,3 +1,39 @@
+def backendDeployStage (String envName) {
+    return {
+        stage("Apply Backend Deployment Config on ${envName}") {
+            timeout(30) {
+                script {
+                    def projectName
+                    def ENV_NAME
+                    def CPU_REQUEST
+                    def CPU_LIMIT
+                    def MEMORY_REQUEST
+                    def MEMORY_LIMIT
+                    if(envName == 'dev') {
+                        projectName = 'tbiwaq-dev'
+                        ENV_NAME = 'dev'
+                        CPU_REQUEST='200m'
+                        CPU_LIMIT='500m'
+                        MEMORY_REQUEST='256Mi'
+                        MEMORY_LIMIT='512Mi'
+                    }
+                    openshift.withProject("${projectName}") {
+                        def backendDCYaml = openshift.process(readFile(file:'openshift/templates/backend/backend-dc-release.yaml'),
+                            "-p",
+                            "ENV_NAME=${ENV_NAME}",
+                            "CPU_REQUEST=${CPU_REQUEST}",
+                            "CPU_LIMIT=${CPU_LIMIT}",
+                            "MEMORY_REQUEST=${MEMORY_REQUEST}",
+                            "MEMORY_LIMIT=${MEMORY_LIMIT}"
+                        )
+                        openshift.apply(backendDCYaml)
+                    }
+                }
+            }
+        }
+    }
+}
+
 def frontendDeployStage (String envName) {
     return {
         stage("Apply Frontend Deployment Config on ${envName}") {
@@ -5,7 +41,6 @@ def frontendDeployStage (String envName) {
                 script {
                     def projectName
                     def ENV_NAME
-                    def DASH_ENV_NAME
                     def CPU_REQUEST
                     def CPU_LIMIT
                     def MEMORY_REQUEST
@@ -16,20 +51,57 @@ def frontendDeployStage (String envName) {
                         DASH_ENV_NAME = '-dev'
                         CPU_REQUEST='100m'
                         CPU_LIMIT='400m'
-                        MEMORY_REQUEST='128Mi'
-                        MEMORY_LIMIT='256Mi'
+                        MEMORY_REQUEST='256Mi'
+                        MEMORY_LIMIT='512Mi'
                     }
                     openshift.withProject("${projectName}") {
                         def frontendDCYaml = openshift.process(readFile(file:'openshift/templates/frontend/frontend-dc-release.yaml'),
-                            "-p",
-                            "ENV_NAME=${ENV_NAME}",
-                            "DASH_ENV_NAME=${DASH_ENV_NAME}",
-                            "CPU_REQUEST=${CPU_REQUEST}",
-                            "CPU_LIMIT=${CPU_LIMIT}",
-                            "MEMORY_REQUEST=${MEMORY_REQUEST}",
-                            "MEMORY_LIMIT=${MEMORY_LIMIT}"
+                                "-p",
+                                "ENV_NAME=${ENV_NAME}",
+                                "CPU_REQUEST=${CPU_REQUEST}",
+                                "CPU_LIMIT=${CPU_LIMIT}",
+                                "MEMORY_REQUEST=${MEMORY_REQUEST}",
+                                "MEMORY_LIMIT=${MEMORY_LIMIT}"
                         )
                         openshift.apply(frontendDCYaml)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+def envoyDeployStage (String envName) {
+    return {
+        stage("Apply Envoy Deployment Config on ${envName}") {
+            timeout(30) {
+                script {
+                    def projectName
+                    def ENV_NAME
+                    def CPU_REQUEST
+                    def CPU_LIMIT
+                    def MEMORY_REQUEST
+                    def MEMORY_LIMIT
+                    if(envName == 'dev') {
+                        projectName = 'tbiwaq-dev'
+                        ENV_NAME = 'dev'
+                        DASH_ENV_NAME = '-dev'
+                        CPU_REQUEST='100m'
+                        CPU_LIMIT='400m'
+                        MEMORY_REQUEST='256Mi'
+                        MEMORY_LIMIT='512Mi'
+                    }
+                    openshift.withProject("${projectName}") {
+                        def envoyDCYaml = openshift.process(readFile(file:'openshift/templates/frontend/envoy-dc-release.yaml'),
+                                "-p",
+                                "ENV_NAME=${ENV_NAME}",
+                                "CPU_REQUEST=${CPU_REQUEST}",
+                                "CPU_LIMIT=${CPU_LIMIT}",
+                                "MEMORY_REQUEST=${MEMORY_REQUEST}",
+                                "MEMORY_LIMIT=${MEMORY_LIMIT}"
+                        )
+                        openshift.apply(envoyDCYaml)
                     }
                 }
             }
