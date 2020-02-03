4e1a154abf14b189de38ad2302f9992595b49572
Richard Tan <richard.tan@gov.bc.ca>
2020-02-03 11:04:01 -0800

    Cleaned up the add organization fixture so it's more readablee

--- backend/api/fixtures/operational/0007_add_organizations.py
+++ backend/api/fixtures/operational/0007_add_organizations.py
@@ -18,246 +18,103 @@ class AddOrganizations(OperationalDataScript):
 
     @transaction.atomic
     def run(self):
-        organization = Organization.objects.create(
-            name="BMW Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="BMW")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
+        list_of_organizations = [{
+            "name": "Aston Martin",
+            "makes": ["Aston Martin"]
+        }, {
+            "name": "BMW Canada Inc.",
+            "makes": ["BMW"]
+        }, {
+            "name": "FCA Canada Inc.",
+            "makes": ["Chrysler", "Fiat"]
+        }, {
+            "name": "Ferrari",
+            "makes": ["Ferrari"]
+        }, {
+            "name": "Ford Motor Company of Canada Ltd.",
+            "makes": ["Ford"]
+        }, {
+            "name": "General Motors of Canada Company",
+            "makes": ["GMC"]
+        }, {
+            "name": "Honda Canada Inc.",
+            "makes": ["Honda"]
+        }, {
+            "name": "Hyundai Canada Inc.",
+            "makes": ["Hyundai"]
+        }, {
+            "name": "Isuzu Commercial Truck of Canada Inc.",
+            "makes": ["Isuzu"]
+        }, {
+            "name": "Jaguar Land Rover Canada ULC",
+            "makes": ["Jaguar"]
+        }, {
+            "name": "Mazda Canada Inc.",
+            "makes": ["Mazda"]
+        }, {
+            "name": "McLaren Automotive Inc.",
+            "makes": []
+        }, {
+            "name": "Mercedes-Benz Canada Inc.",
+            "makes": ["Mercedes-Benz"]
+        }, {
+            "name": "Renault-Nissan-Mitsubishi Alliance",
+            "makes": ["Mitsubishi", "Nissan"]
+        }, {
+            "name": "Renault-Nissan-Mitsubishi Alliance",
+            "makes": ["Mitsubishi", "Nissan"]
+        }, {
+            "name": "Pagani",
+            "makes": []
+        }, {
+            "name": "PSA",
+            "makes": []
+        }, {
+            "name": "Subaru Canada Inc.",
+            "makes": ["Subaru"]
+        }, {
+            "name": "Suzuki",
+            "makes": ["Suzuki"]
+        }, {
+            "name": "Tesla Canada GP Inc.",
+            "makes": ["Tesla"]
+        }, {
+            "name": "Toyota Canada Inc.",
+            "makes": ["Toyota"]
+        }, {
+            "name": "Volkswagen Group Canada Inc.",
+            "makes": ["Porsche", "Volkswageen"]
+        }, {
+            "name": "Volvo Car Canada Ltd.",
+            "makes": ["Volvo"]
+        }]
 
-        organization = Organization.objects.create(
-            name="Mercedes-Benz Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Mercedes-Benz")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
+        organizations_added = 0
 
-        organization = Organization.objects.create(
-            name="FCA Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Chrysler")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-        (make, _created) = Make.objects.get_or_create(name="Fiat")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
+        for organization in list_of_organizations:
+            organization_name = organization.get("name")
+            (organization_model, created) = Organization.objects.get_or_create(
+                name=organization_name,
+                defaults={
+                    'is_active': True,
+                    'is_government': False
+                }
+            )
 
-        organization = Organization.objects.create(
-            name="Ford Motor Company of Canada Ltd.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Ford")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
+            if created:
+                organizations_added += 1
 
-        organization = Organization.objects.create(
-            name="General Motors of Canada Company",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="GMC")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
+            list_of_makes = organization.get("makes")
 
-        organization = Organization.objects.create(
-            name="Honda Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Honda")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
+            for make_name in list_of_makes:
+                (make, _) = Make.objects.get_or_create(name=make_name)
 
-        organization = Organization.objects.create(
-            name="Hyundai Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Hyundai")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
+                VehicleMakeOrganization.objects.get_or_create(
+                    organization=organization_model,
+                    vehicle_make=make
+                )
 
-        organization = Organization.objects.create(
-            name="Mazda Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Mazda")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="Renault-Nissan-Mitsubishi Alliance",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Mitsubishi")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        (make, _created) = Make.objects.get_or_create(name="Nissan")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-        organization = Organization.objects.create(
-            name="PSA",
-            is_active=True,
-            is_government=False
-        )  # no makes yet for PSA
-
-        organization = Organization.objects.create(
-            name="Subaru Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Subaru")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="Suzuki",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Suzuki")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="Jaguar Land Rover Canada ULC",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Jaguar")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="Tesla Canada GP Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Tesla")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="Toyota Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Toyota")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="Volkswagen Group Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Volkswagen")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-        (make, _created) = Make.objects.get_or_create(name="Porsche")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="Volvo Car Canada Ltd.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Volvo")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="Ferrari",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Ferrari")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-        organization = Organization.objects.create(
-            name="Aston Martin",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Aston Martin")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="McLaren Automotive Inc.",
-            is_active=True,
-            is_government=False
-        )  # no makes yet for Mclaren
-
-        organization = Organization.objects.create(
-            name="Isuzu Commercial Truck of Canada Inc.",
-            is_active=True,
-            is_government=False
-        )
-        (make, _created) = Make.objects.get_or_create(name="Isuzu")
-        VehicleMakeOrganization.objects.create(
-            organization=organization,
-            vehicle_make=make
-        )
-
-        organization = Organization.objects.create(
-            name="Pagani",
-            is_active=True,
-            is_government=False
-        )  # no makes yet for Pagani
+        print("Added {} organizations.".format(organizations_added))
 
 
 script_class = AddOrganizations
