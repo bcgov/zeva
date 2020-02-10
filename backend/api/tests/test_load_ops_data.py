d0bb6da041e8ff9951558435ad135da99a738c19
Richard Tan <richard.tan@gov.bc.ca>
2020-02-10 15:30:28 -0800

    Fixed load_ops_data tests

--- backend/api/tests/test_load_ops_data.py
+++ backend/api/tests/test_load_ops_data.py
@@ -11,17 +11,25 @@ class TestLoadOpsData(TestCase):
     Execute specified operational scripts to validate that they work
     """
 
-    ScriptDefinition = namedtuple('ScriptDefinition', ('file', 'args', 'skip'), defaults=('', False,))
+    ScriptDefinition = namedtuple('ScriptDefinition', ('file', 'args', 'skip'))
 
     scripts = [
-        ScriptDefinition('api.fixtures.operational.0000_add_government_organization'),
-        ScriptDefinition('api.fixtures.operational.0001_add_vehicle_makes'),
-        ScriptDefinition('api.fixtures.operational.0002_add_vehicle_classes'),
-        ScriptDefinition('api.fixtures.operational.0003_add_vehicle_fuel_types'),
-        ScriptDefinition('api.fixtures.operational.0004_add_model_years'),
-        ScriptDefinition('api.fixtures.operational.0005_add_plugin_hybrid_vehicles'),
-        ScriptDefinition('api.fixtures.operational.0006_add_battery_electric_vehicles'),
-        ScriptDefinition('api.fixtures.operational.0007_add_organizations')
+        ScriptDefinition(
+            'api.fixtures.operational.0000_add_government_organization', '', False),
+        ScriptDefinition(
+            'api.fixtures.operational.0001_add_vehicle_makes', '', False),
+        ScriptDefinition(
+            'api.fixtures.operational.0002_add_vehicle_classes', '', False),
+        ScriptDefinition(
+            'api.fixtures.operational.0003_add_vehicle_fuel_types', '', False),
+        ScriptDefinition(
+            'api.fixtures.operational.0004_add_model_years', '', False),
+        ScriptDefinition(
+            'api.fixtures.operational.0005_add_organizations', '', False),
+        ScriptDefinition(
+            'api.fixtures.test.0001_add_plugin_hybrid_vehicles', '', False),
+        ScriptDefinition(
+            'api.fixtures.test.0002_add_battery_electric_vehicles', '', False),
     ]
 
     logger = logging.getLogger('zeva.test')
