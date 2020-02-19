96c617529b615e32ed39457a3d20181ab5a40fb2
Kuan Fan <kuan.fan@gov.bc.ca>
2020-02-19 12:13:12 -0800

    update

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -45,6 +45,33 @@ module.exports = settings => {
     }
   }))
 
+  // deploy backend
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/backend/backend-dc.yaml`, {
+    'param':{
+      'NAME': phases[phase].name,
+      'SUFFIX': phases[phase].suffix,
+      'VERSION': phases[phase].tag,
+      'ENV_NAME': phases[phase].phase,
+      'CPU_REQUEST': '100m',
+      'CPU_LIMIT': '500m',
+      'MEMORY_REQUEST': '1100M',
+      'MEMORY_LIMIT': '2G'
+    }
+  }))
+  // deploy database
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/postgresql/postgresql-dc.yaml`, {
+    'param':{
+      'NAME': phases[phase].name,
+      'SUFFIX': phases[phase].suffix,
+      'VERSION': phases[phase].tag,
+      'ENV_NAME': phases[phase].phase,
+      'CPU_REQUEST': '100m',
+      'CPU_LIMIT': '500m',
+      'MEMORY_REQUEST': '200M',
+      'MEMORY_LIMIT': '500M'
+    }
+  }))
+
   oc.applyRecommendedLabels(
     objects,
     phases[phase].name,
