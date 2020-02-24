7a661ee69e8834386b2ad8e9b567c53cf1a3f486
Robert Johnstone <rob@plasticviking.com>
2020-02-24 15:17:06 +0800

    sales data model

---
+++ backend/api/models/record_of_sale.py
@@ -0,0 +1,78 @@
+from api.models.record_of_sale_statuses import RecordOfSaleStatuses
+from api.models.vin_statuses import VINStatuses
+from auditable.models import Auditable
+from django.db import models
+from enumfields import EnumField
+
+
+class RecordOfSale(Auditable):
+    organization = models.ForeignKey(
+        'Organization',
+        related_name='sales',
+        on_delete=models.CASCADE
+    )
+
+    vehicle = models.ForeignKey(
+        'Vehicle',
+        related_name='sales',
+        on_delete=models.CASCADE
+    )
+
+    sale_id = models.CharField(blank=True,
+                               null=True,
+                               max_length=255,
+                               db_comment="An (optional) vehicle-supplier-provided correlation ID")
+
+    vin = models.CharField(blank=True,
+                           null=True,
+                           max_length=255,
+                           db_comment="An (optional) vehicle-supplier-provided correlation ID")
+
+    vin_validation_status = EnumField(
+        VINStatuses,
+        max_length=30,
+        null=False,
+        default=VINStatuses.UNCHECKED,
+        db_comment="The validation status of the VIN. Valid statuses: "
+                   "{statuses}".format(
+            statuses=[c.name for c in VINStatuses]
+        )
+    )
+
+    sale_date = models.DateField(blank=True,
+                                 null=True,
+                                 db_comment="The calendar date the vehicle was placed with an end customer")
+
+    submission_date = models.DateField(
+        blank=True,
+        null=True,
+        auto_now=True,
+        db_comment="The calendar date the submission was entered in ZEVA"
+    )
+
+    validation_status = EnumField(
+        RecordOfSaleStatuses,
+        max_length=20,
+        null=False,
+        default=VINStatuses.UNCHECKED,
+        db_comment="The validation status of this record of sale. Valid statuses: "
+                   "{statuses}".format(
+            statuses=[c.name for c in RecordOfSaleStatuses]
+        )
+    )
+
+    # @todo we don't have a transaction table yet, but this should hold a reference to any credit transaction created
+    # transaction = models.ForeignKey(
+    #         'CreditTransaction',
+    #         related_name='sale',
+    #         on_delete=models.PROTECT,
+    #         null=True,
+    #     )
+
+    class Meta:
+        db_table = "record_of_sale"
+        unique_together = [['organization', 'sale_id']]
+
+    db_table_comment = 'Hold individual records of sale submitted by ZEV suppliers, including their VIN, ' \
+                       'sales dates, and the workflow state information for this sale (VIN and record validation ' \
+                       'statuses)'
---
+++ backend/api/models/record_of_sale_statuses.py
@@ -0,0 +1,11 @@
+from enum import Enum, unique
+
+
+@unique
+class RecordOfSaleStatuses(Enum):
+    NEW = 'NEW'
+    DRAFT = 'DRAFT'
+    SUBMITTED = 'SUBMITTED'
+    VALIDATED = 'VALIDATED'
+    REJECTED = 'REJECTED'
+    TRANSACTED = 'TRANSACTED'
