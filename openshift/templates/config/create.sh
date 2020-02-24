59bf9a428ccd71217b91672776c210bc9fd30ef7
Kuan Fan <kuan.fan@gov.bc.ca>
2020-02-24 13:08:05 -0800

    tag a new postgresql 10

--- openshift/templates/config/create.sh
+++ openshift/templates/config/create.sh
@@ -1,4 +1,13 @@
-## create configmap
+## create database image in tools project
+## the image fixt defect used to initialize database properly
+## zeva uses this image to create extension hstore and others
+oc tag registry.access.redhat.com/rhscl/postgresql-10-rhel7:1-47 tbiwaq-tools/postgresql:10-1-47
+
+## create configmap to initialize database properly
+oc process -f zeva-postgresql-init.yaml | oc apply -f - -n tbiwaq-dev --dry-run=true
+oc process -f zeva-postgresql-init.yaml | oc apply -f - -n tbiwaq-test --dry-run=true
+oc process -f zeva-postgresql-init.yaml | oc apply -f - -n tbiwaq-prod --dry-run=true
+
 oc process -f configmap.yaml ENV_NAME=dev SSO_NAME=sso-dev KEYCLOAK_REALM=rzh2zkjq | oc apply -f - -n tbiwaq-dev --dry-run=true
 
 ## Create secretes for rabbitmq
