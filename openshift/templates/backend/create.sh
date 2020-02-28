89c0082ceafb2e72690b40ac244823ee399e1b1f
Kuan Fan <kuan.fan@gov.bc.ca>
2020-02-28 14:56:25 -0800

    update python base image

--- openshift/templates/backend/create.sh
+++ openshift/templates/backend/create.sh
@@ -1,6 +1,6 @@
 
 ### build backend
-oc process -f ./backend-bc-release.yaml ZEVA_RELEASE=pipeline-release4 | oc apply -f - -n tbiwaq-tools --dry-run=true
+oc process -f ./backend-bc.yaml NAME=zeva SUFFIX=-dev-96 VERSION=1.0.0-96 GIT_URL=https://github.com/bcgov/zeva.git GIT_REF=refs/pull/96/head | oc apply -f - -n tbiwaq-tools --dry-run=true
 
 ### tag dev image
 oc tag tbiwaq-tools/backend:pipeline-release4 tbiwaq-dev/backend:dev
@@ -9,3 +9,7 @@ oc tag tbiwaq-tools/envoy:pipeline-release4 tbiwaq-dev/envoy:dev
 
 ### deploy dev image
 oc process -f ./backend-dc-release.yaml ENV_NAME=dev CPU_REQUEST=100m CPU_LIMIT=500m MEMORY_REQUEST=512Mi MEMORY_LIMIT=2Gi | oc apply -f - -n tbiwaq-dev --dry-run=true
+
+
+
+oc process -f ./backend-bc-copy.yaml NAME=zeva SUFFIX=-dev-96 VERSION=1.0.0-96 GIT_URL=https://github.com/bcgov/zeva.git GIT_REF=refs/pull/96/head | oc apply -f - -n tbiwaq-tools --dry-run=true
