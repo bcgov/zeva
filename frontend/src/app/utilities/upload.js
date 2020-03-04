022bdadc612866ff7d3460a5bfdfb27d317810a3
Robert Johnstone <rob@plasticviking.com>
2020-03-04 15:28:04 +0800

    eslint sanity fix

--- frontend/src/app/utilities/upload.js
+++ frontend/src/app/utilities/upload.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 const upload = (url, files) => {
   const data = new FormData();
 
-  // @todo seriously, eslint. no plusplus? you and I are going to have a talk when I figure out exactly what to suppress
-  for (let i = 0; i < files.length; i += 1) {
+  for (let i = 0; i < files.length; i++) {
     data.append('files', files[i]);
   }
 
