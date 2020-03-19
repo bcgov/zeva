88f43a622e43019a5c9f7b2445c5185ce70a7003
emi-hi <emily.hillier@gmail.com>
2020-03-19 11:13:52 -0700

    styled table

--- frontend/src/credits/components/CreditTransactions.js
+++ frontend/src/credits/components/CreditTransactions.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import ReactTable from 'react-table';
 const CreditTransactions = (props) => {
-  const { title } = props;
+  const { title, items } = props;
+  const columns = [{
+    accessor: 'displayName',
+    className: 'text-left',
+    Header: 'Name',
+  }, {
+    accessor: (item) => (item.groups.join(', ')),
+    id: 'roles',
+    className: 'text-left',
+    Header: 'Roles',
+  }, {
+    accessor: (item) => (item.isActive ? 'Active' : 'Inactive'),
+    className: 'text-center',
+    Header: 'Status',
+    id: 'status',
+  }];
+
+  // const filterMethod = (filter, row) => {
+  //   const id = filter.pivotId || filter.id;
+  //   return row[id] !== undefined ? String(row[id])
+  //     .toLowerCase()
+  //     .includes(filter.value.toLowerCase()) : true;
+  // };
+
+  // const filterable = true;
+
   return (
-    <b> {title}</b>
+    <div id="credit-transaction-details" className="page">
+      <div className="row">
+        <div className="col-sm-12">
+          <h2>{title}</h2>
+          <p>Credit Balance: add later</p>
+        </div>
+      </div>
+      <ReactTable
+        className="searchable"
+        columns={columns}
+        data={items}
+        // defaultFilterMethod={filterMethod}
+        defaultPageSize={10}
+        defaultSorted={[{
+          id: 'displayName',
+        }]}
+        // filterable={filterable}
+        getTrProps={(state, row) => {
+          if (row && row.original) {
+            return {
+              onClick: () => {
+                const { id } = row.original;
+                history.push(`/users/${id}`);
+              },
+              className: 'clickable',
+            };
+          }
+
+          return {};
+        }}
+        pageSizeOptions={[5, 10, 15, 20, 25, 50, 100]}
+      />
+    </div>
   );
 };
-
 export default CreditTransactions;
