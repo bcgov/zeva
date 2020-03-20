9d3eaf79694c24242555d7665efcdaae483b3e9f
emi-hi <emily.hillier@gmail.com>
2020-03-20 14:01:28 -0700

    fixed key and nesting errors, added margin to balance <p>

--- frontend/src/credits/components/CreditTransactions.js
+++ frontend/src/credits/components/CreditTransactions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactTable from 'react-table';
+import { forEachChild } from 'typescript';
 
 const CreditTransactions = (props) => {
   const { title, items } = props;
@@ -10,15 +11,17 @@ const CreditTransactions = (props) => {
       <div className="row">
         <div className="col-sm-12">
           <h2>{title}</h2>
-          <p>Credit Balance: add later</p>
+          <p>Credit Balance:</p>
         </div>
       </div>
       <table className="transaction-table">
         <thead>
-          <th colSpan="1" className="date-column"> </th>
-          <th colSpan="1"> </th>
-          <th colSpan="2">Credits</th>
-          <th colSpan="2" className="balance-a">Balance</th>
+          <tr>
+            <th colSpan="1" className="date-column"> </th>
+            <th colSpan="1"> </th>
+            <th colSpan="2">Credits</th>
+            <th colSpan="2" className="balance-a">Balance</th>
+          </tr>
           <tr>
             <th>Date</th>
             <th>Transaction</th>
@@ -30,8 +33,8 @@ const CreditTransactions = (props) => {
         </thead>
         <tbody>
           {items.map((each) => (
-            <tr>
-              <td className="date-column">{each.transactionTimestamp.slice(0,10)}</td>
+            <tr key={each.id}>
+              <td className="date-column">{each.transactionTimestamp.slice(0, 10)}</td>
               <td className="text-left">{each.transactionType.transactionType}</td>
               <td className="text-center">1</td>
               <td className="text-center">1</td>
