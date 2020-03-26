8fc2ca95fbfca943c3cd47ef42a6a6c668e76cd5
Kuan Fan <kuan.fan@gov.bc.ca>
2020-03-26 14:55:02 -0700

    add creating extensions

--- openshift/templates/patroni/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
+++ openshift/templates/patroni/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
@@ -8,6 +8,9 @@ if [[ (! -z "$APP_USER") &&  (! -z "$APP_PASSWORD") && (! -z "$APP_DATABASE")]];
   echo "Creating database ${APP_DATABASE}"
   psql "$1" -w -c "CREATE DATABASE ${APP_DATABASE} OWNER ${APP_USER} ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
 
+  echo "Creating extensions 1 is $1" 
+  psql -U postgres -q -d "${APP_DATABASE}" -c 'create extension if not exists hstore'
+
 else
   echo "Skipping user creation"
   echo "Skipping database creation"
