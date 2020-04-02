9e4a9711578893c825db1f732d78c701b1bee292
Emily <44536222+emi-hi@users.noreply.github.com>
2020-04-02 11:48:29 -0700

    zeva 110 - upload icbc spreadsheet (front end) (#135)
    * added icbc table model
    
    * changed table name and comment
    
    * started icbc upload component
    
    * made vin unique, not null, not blank
    
    * added upload page for verification
    
    * icbc verification data file drop, added max limit to excelfiledrop, added a default of 10000 files to sales submission so that it doesn't give error on that prop

--- frontend/src/app/routes/Credits.js
+++ frontend/src/app/routes/Credits.js
@@ -1,6 +1,7 @@
 const API_BASE_PATH = '/credit-transactions';
 
 const CREDITS = {
+  UPLOADVERIFICATION: `${API_BASE_PATH}/upload-verification-data`,
   LIST: `${API_BASE_PATH}`,
 };
 
---
+++ frontend/src/credits/components/UploadVerificationData.js
@@ -0,0 +1,146 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import history from '../../app/History';
+import CustomPropTypes from '../../app/utilities/props';
+import ExcelFileDrop from '../../sales/components/ExcelFileDrop';
+
+const UploadVerificationData = (props) => {
+  const {
+    title,
+    errorMessage,
+    files,
+    setUploadFiles,
+    upload,
+    setDateCurrentTo,
+    dateCurrentTo,
+  } = props;
+
+  const getFileSize = (bytes) => {
+    if (bytes === 0) {
+      return '0 bytes';
+    }
+
+    const k = 1000;
+    const sizes = ['bytes', 'KB', 'MB', 'GB', 'TB'];
+    let i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    if (i > 4) { // nothing bigger than a terrabyte
+      i = 4;
+    }
+
+    const filesize = parseFloat((bytes / k ** i).toFixed(1));
+
+    return `${filesize} ${sizes[i]}`;
+  };
+
+  const removeFile = (removedFile) => {
+    const found = files.findIndex((file) => (file === removedFile));
+    files.splice(found, 1);
+    setUploadFiles([...files]);
+  };
+
+  const handleCalendarChange = (event) => {
+    const { value } = event.target;
+    setDateCurrentTo(value);
+  };
+
+  return (
+    <div id="sales-edit" className="page">
+      <h2>{title}</h2>
+      <p>ICBC data current to: {dateCurrentTo}</p>
+      <div className="compact">
+        <div className="bordered">
+          <div className="content">
+            <div className="panel panel-default">
+              <ExcelFileDrop setFiles={setUploadFiles} maxFiles={1} />
+              <div className="files">
+                <div className="row">
+                  <div className="col-7 header"><label htmlFor="filename">Filename</label></div>
+                  <div className="col-3 size header"><label htmlFor="filesize">Size</label></div>
+                  {/* <div className="col-2 size header"><label htmlFor="securityscan">Security<br />Scan</label></div> */}
+                  <div className="col-2 actions header" />
+                </div>
+                {files.map((file) => (
+                  <div className="row" key={file.name}>
+                    <div className="col-7">{file.name}</div>
+                    <div className="col-3 size">{getFileSize(file.size)}</div>
+                    {/* <div className="col-2 size">
+                      <div className="check" type="button">
+                        <FontAwesomeIcon icon="check" />
+                      </div>
+                    </div> */}
+                    <div className="col-2 actions">
+                      <button
+                        className="delete"
+                        onClick={() => {
+                          removeFile(file);
+                        }}
+                        type="button"
+                      >
+                        <FontAwesomeIcon icon="trash" />
+                      </button>
+                    </div>
+                  </div>
+                ))}
+
+                <div>
+                  <div className="row">
+                    <label id="date-label" htmlFor="current-to">Date current to:</label>
+                    <div>
+                      <input
+                        type="date"
+                        id="date-current-to"
+                        name="date-current-to"
+                        onChange={handleCalendarChange}
+                      />
+                      <FontAwesomeIcon icon="calendar-alt" className="calendar-icon" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="action-bar">
+                <span className="left-content">
+                  <button
+                    className="button"
+                    onClick={() => {
+                      history.goBack();
+                    }}
+                    type="button"
+                  >
+                    <FontAwesomeIcon icon="arrow-left" /> Back
+                  </button>
+                </span>
+
+                <span className="right-content">
+                  <button
+                    disabled={files.length === 0}
+                    className="button primary"
+                    onClick={() => upload()}
+                    type="button"
+                  >
+                    <FontAwesomeIcon icon="upload" /> Upload
+                  </button>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="clearfix" />
+    </div>
+  );
+};
+
+UploadVerificationData.propTypes = {
+  files: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  setUploadFiles: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  upload: PropTypes.func.isRequired,
+  setDateCurrentTo: PropTypes.func.isRequired,
+  dateCurrentTo: PropTypes.string.isRequired,
+};
+
+export default UploadVerificationData;
