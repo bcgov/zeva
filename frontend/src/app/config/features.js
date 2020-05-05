031f6225d83dc3ac3d7cd53215a300b18b037421
Richard <Richard.Tan@gov.bc.ca>
2020-05-05 06:21:50 -0700

    ZEVA-135: Configurable modules (#160)
    * Added configurable features
    
    * Moved configurable features to a separate folder
    
    * Fixed merge conflicts

---
+++ frontend/src/app/config/features.js
@@ -0,0 +1,8 @@
+window.zeva_config = {
+  'credit_transfers.enabled': false,
+  'credit_transactions.enabled': true,
+  'initiative_agreements.enabled': false,
+  'model_year_report.enabled': false,
+  'purchase_requests.enabled': false,
+  'roles.enabled': false,
+};
