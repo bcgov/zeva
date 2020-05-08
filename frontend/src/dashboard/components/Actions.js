1cf01e415dae8bc743005395c304eb010f6b32e8
emi-hi <emily.hillier@gmail.com>
2020-05-08 12:05:56 -0700

    homepage styling, moved organization details info into user component

--- frontend/src/dashboard/components/Actions.js
+++ frontend/src/dashboard/components/Actions.js
@@ -1,88 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+import History from '../../app/History';
+import ROUTES_VEHICLES from '../../app/routes/Vehicles';
+import Loading from '../../app/components/Loading';
+import CustomPropTypes from '../../app/utilities/props';
 
-const Actions = () => (
-  <div id="actions" className="dashboard-fieldset">
-    <h1>Actions</h1>
+const Actions = (props) => {
+  const { details } = props;
+  const [loading, setLoading] = useState(true);
+  const [validatedCount, setValidatedCount] = useState(0);
+  const refreshList = () => {
+    axios.get(ROUTES_VEHICLES.LIST).then((response) => {
+      const validatedVehicles = response.data
+        .filter((vehicle) => vehicle.validationStatus === 'SUBMITTED')
+        .map((vehicle) => vehicle.modelName);
+      setValidatedCount(validatedVehicles.length);
+      setLoading(false);
+    });
+  };
+  useEffect(() => {
+    refreshList(true);
+  }, []);
 
-    <div className="content">
-      <div className="value">
-        3
-      </div>
-
-      <div className="text">
-        <div>
-          <button type="button">Credit transfer(s) in progress</button>
-          <span className="divider">|</span>
-          <button type="button">View all transactions</button>
-        </div>
-
-        <FontAwesomeIcon icon={['fas', 'play']} />
-        <button type="button"> Start a new credit transfer proposal</button>
-      </div>
-    </div>
-
-    <div className="content">
-      <div className="value">
-        1
-      </div>
-
-      <div className="text">
-        <div>
-          <button type="button">Compliance report(s) in progress</button>
-          <span className="divider">|</span>
-          <button type="button">View all reports</button>
-        </div>
-
-        <FontAwesomeIcon icon={['fas', 'play']} />
-        <button type="button"> Start a new compliance report</button>
-      </div>
-    </div>
-
-    <div className="content">
-      <div className="value">
-        <div className="icon">
-          <FontAwesomeIcon icon="lock" className="icon-secure" />
-          <FontAwesomeIcon icon="upload" className="icon-upload" />
-        </div>
-      </div>
-
-      <div className="text">
-        <div>
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div id="actions" className="dashboard-fieldset">
+      <h1>Actions</h1>
+      <h5 id="action-org-na">{details.organization.name} has:</h5>
+      <div className="content">
+        <div className="text">
+          <div>
+            <span className="value"> {validatedCount}</span>
+            <button type="button">
+             ZEV Models submitted for Validation
+            </button>
+          </div>
+          <FontAwesomeIcon icon={['fas', 'play']} />
           <button
             type="button"
-          >
-            Submit/Upload a file for an Initiative Agreement
+            onClick={() => { History.push(ROUTES_VEHICLES.LIST); }}
+          > Manage ZEV model lineup
           </button>
         </div>
-
-        <FontAwesomeIcon icon={['fas', 'play']} />
-        <button type="button"> Initiative Agreement application information</button>
       </div>
     </div>
-
-    <div className="content">
-      <div className="text">
-        <button type="button">
-          Enter sales data
-        </button>
-      </div>
-    </div>
-
-    <div className="content">
-      <div className="text">
-        <button type="button">
-          Manage ZEV model lineup
-        </button>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 Actions.defaultProps = {
 };
 
 Actions.propTypes = {
+  details: CustomPropTypes.organizationDetails.isRequired,
 };
 
 export default Actions;
