2888186e1698ef533ed5008a9b9eafe693d7f1c1
Emily <44536222+emi-hi@users.noreply.github.com>
2020-05-12 12:07:05 -0700

    ZEVA 145: Home page tidy up (#169)
    * commented out credits, org class, and class on org details page and navbar
    
    * adds create serializer for new organization, adds logic in supplierdetailscontainer to check for id (existing) or not (new) and loads form accordingly. Adds axios post for new organization instead of patch
    
    * added boolean for new supplier route instead of string
    
    * homepage styling, moved organization details info into user component
    
    * removed symtable

--- frontend/src/dashboard/components/DashboardPage.js
+++ frontend/src/dashboard/components/DashboardPage.js
@@ -2,31 +2,21 @@ import React from 'react';
 import CustomPropTypes from '../../app/utilities/props';
 
 import Actions from './Actions';
-import Balance from './Balance';
 import Feedback from './Feedback';
-import OrganizationDetails from './OrganizationDetails';
 import UserSettings from './UserSettings';
 
 const DashboardPage = (props) => {
   const { user } = props;
-
   return (
     <div id="dashboard">
       <div className="row">
         <div className="col-lg-3">
-          <Balance organization={user.organization} />
-
+          <UserSettings details={user} />
           <Feedback />
         </div>
 
-        <div className="col-lg-5">
-          <Actions />
-        </div>
-
-        <div className="col-lg-4">
-          <OrganizationDetails details={user.organization} />
-
-          <UserSettings details={user} />
+        <div className="col-lg-9">
+          <Actions details={user} />
         </div>
       </div>
     </div>
--- frontend/src/dashboard/components/UserSettings.js
+++ frontend/src/dashboard/components/UserSettings.js
@@ -1,60 +1,65 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import History from '../../app/History';
 import CustomPropTypes from '../../app/utilities/props';
+import ROUTES_USERS from '../../app/routes/Users';
+import ROUTES_ORGANIZATIONS from '../../app/routes/Organizations';
 
 const UserSettings = (props) => {
   const { details } = props;
   return (
     <div id="user-settings" className="dashboard-fieldset">
-      <h1>User Settings</h1>
+      <h1>Welcome</h1>
 
       <div className="content">
         <div className="text">
-          {details.displayName}, Senior Signing Officer
+          <h5>{details.displayName}{details.title ? ', ' : ''}</h5>
+          <dd>{details.title}</dd>
         </div>
       </div>
 
       <div className="content">
         <div className="text">
-          <button type="button">
-            User profile
+          <FontAwesomeIcon icon={['fas', 'play']} />
+          <button
+            type="button"
+            onClick={() => {
+              History.push(ROUTES_USERS.EDIT.replace(/:id/gi, details.id));
+            }}
+          >
+            Edit Profile/Users
           </button>
         </div>
       </div>
-
-      <div className="content">
-        <div className="value">
-          6
-        </div>
-
-        <div className="text">
-          <button type="button">
-            Notifications
-          </button>
-        </div>
-        <span className="icon">
-          <FontAwesomeIcon icon={['far', 'bell']} />
-        </span>
-      </div>
-
       <div className="content">
         <div className="text">
-          <button type="button">
-            Configure your notifications
-          </button>
+          <h5>{details.organization.name}</h5>
+          {details.organization.organizationAddress && (
+            <dl>
+              <dd>{details.organization.organizationAddress.addressLine1}</dd>
+              <dt />
+              <dd>
+                <span> {details.organization.organizationAddress.city} </span>
+                <span> {details.organization.organizationAddress.state} </span>
+                <span> {details.organization.organizationAddress.country} </span>
+              </dd>
+              <dt />
+              <dd>{details.organization.organizationAddress.postalCode}</dd>
+            </dl>
+          )}
         </div>
       </div>
 
       <div className="content">
         <div className="text pdf-link">
-          <a
-            href="/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Help
-            <FontAwesomeIcon icon={['far', 'file-pdf']} />
-          </a>
+          <FontAwesomeIcon icon={['fas', 'play']} />
+          <button
+            type="button"
+            onClick={() => {
+              History.push(ROUTES_ORGANIZATIONS.EDIT.replace(/:id/gi, details.organization.id));
+            }}
+          > Edit Vehicle Supplier Info
+          </button>
         </div>
       </div>
     </div>
