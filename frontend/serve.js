6257ab1eac9eca589fa27fbd07a1bd2f544bee52
Richard Tan <richard.tan@gov.bc.ca>
2020-06-01 12:07:45 -0700

    Creating an exception for api. so that when we deploy we don't have to wait for 10 mins for the routers to kick in

--- frontend/serve.js
+++ frontend/serve.js
@@ -13,10 +13,21 @@ const io = require('socket.io')(websocketServer);
 notifications.setup(io);
 websocketServer.listen(5002, '0.0.0.0');
 
+const unless = (path, middleware) => (
+  (req, res, next) => {
+    if (req.originalUrl.indexOf(path) === 0) {
+      return next();
+    }
+
+    return middleware(req, res, next);
+  }
+);
+
 const app = express();
-app.use(morgan('combined'));
+app.use(unless('/api', morgan('combined')));
 
 const root = `${__dirname}/public/build`;
 app.use(express.static(root));
 app.use(fallback('generated_index.html', { root }));
+
 app.listen(3000);
