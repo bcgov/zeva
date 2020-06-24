d96e08e200a09d9a6e17137db2ec702f6dba7420
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2020-06-24 14:25:51 -0700

    Only load test data for dev and test env (#228)

--- backend/load_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python ./manage.py load_ops_data --directory ./api/fixtures/operational
-if [ $ENV_NAME == 'dev' ] || [ $ENV_NAME == 'test' ]; then
-  python ./manage.py load_ops_data --directory ./api/fixtures/test
-fi
