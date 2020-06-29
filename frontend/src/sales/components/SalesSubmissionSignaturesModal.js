b0c893c9f9a3ab85a3a4e69a27a31d947575e45c
Richard Tan <richard.tan@gov.bc.ca>
2020-06-29 09:34:16 -0700

    Increased width for the modal window for Sales Validation

--- frontend/src/sales/components/SalesSubmissionSignaturesModal.js
+++ frontend/src/sales/components/SalesSubmissionSignaturesModal.js
@@ -14,21 +14,26 @@ const SalesSubmissionSignaturesModal = (props) => {
       confirmLabel="Sign and Submit"
       handleCancel={handleCancel}
       handleSubmit={handleSubmit}
+      modalClass="w-75"
       showModal={showModal}
       title="Signing Authority Declaration"
     >
       <strong>I, {user.displayName}, senior Signing Officer, {user.organization.name}:</strong>
       <div>
-        <input type="checkbox" id="sd-1" />
-        <label htmlFor="sd-1"> confirm that records
+        <div className="d-inline-block align-middle my-2 mr-2">
+          <input type="checkbox" id="sd-1" />
+        </div>
+        <label className="d-inline" htmlFor="sd-1"> confirm that records
           evincing each matter
           reported under section 11.11 (2) of the Regulation
           are available on request.
         </label>
       </div>
       <div>
-        <input type="checkbox" id="sd-2" />
-        <label htmlFor="sd-2"> confirm that I am an
+        <div className="d-inline-block align-middle my-2 mr-2">
+          <input type="checkbox" id="sd-2" />
+        </div>
+        <label className="d-inline" htmlFor="sd-2"> confirm that I am an
           officer or employee of
           the vehicle supplier, and that records evidencing my authority to
           submit this proposal are available on
@@ -36,8 +41,10 @@ const SalesSubmissionSignaturesModal = (props) => {
         </label>
       </div>
       <div>
-        <input type="checkbox" id="sd-3" />
-        <label htmlFor="sd-3"> certify that the
+        <div className="d-inline-block align-middle my-2 mr-2">
+          <input type="checkbox" id="sd-3" />
+        </div>
+        <label className="d-inline" htmlFor="sd-3"> certify that the
           information in this report
           is true and complete to the best of my knowledge and I
           understand that the Director may require records
