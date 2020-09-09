316896d27a45fd3b30c1ff004d2913679cda999e
emi-hi <emily.hillier@gmail.com>
2020-09-09 08:42:48 -0700

    added conditional to displaying records and service addres so that it doesn't give error

--- frontend/src/sales/components/SalesSubmissionContentPage.js
+++ frontend/src/sales/components/SalesSubmissionContentPage.js
@@ -137,10 +137,10 @@ const SalesSubmissionContentPage = (props) => {
           <h2>Application for Credits for Consumer Sales</h2>
           <h3>{user.organization.name}</h3>
           <h5 className="d-inline sales-upload-grey mr-5">Service address: </h5>
-          <h5 className="d-inline sales-upload-blue">{serviceAddress.addressLine1} {serviceAddress.city} {serviceAddress.state} {serviceAddress.postalCode}</h5>
+          {serviceAddress && <h5 className="d-inline sales-upload-blue">{serviceAddress.addressLine1} {serviceAddress.city} {serviceAddress.state} {serviceAddress.postalCode}</h5>}
           <br />
           <h5 className="d-inline sales-upload-grey mr-5">Records address: </h5>
-          <h5 className="d-inline sales-upload-blue">{recordsAddress.addressLine1} {recordsAddress.city} {recordsAddress.state} {recordsAddress.postalCode}</h5>
+          {recordsAddress && <h5 className="d-inline sales-upload-blue">{recordsAddress.addressLine1} {recordsAddress.city} {recordsAddress.state} {recordsAddress.postalCode}</h5>}
           <p className="font-weight-bold my-3">
             Consumer Sales: {content.length}
           </p>
