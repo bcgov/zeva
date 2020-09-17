53c471a7ba2de45c02912fad2f3ae388da4440c5
emi-hi <emily.hillier@gmail.com>
2020-09-17 14:15:15 -0700

    added condition for icbc upload date display

--- frontend/src/credits/CreditRequestDetailsContainer.js
+++ frontend/src/credits/CreditRequestDetailsContainer.js
@@ -29,7 +29,9 @@ const CreditRequestDetailsContainer = (props) => {
       axios.get(ROUTES_SALES_SUBMISSIONS.DETAILS.replace(':id', id)),
       axios.get(ROUTES_SALES_SUBMISSIONS.RAW.replace(':id', id)),
     ]).then(axios.spread((dateResponse, submissionResponse, rawResponse) => {
+      if (dateResponse.data.uploadDate) {
       setPreviousDateCurrentTo(dateResponse.data.uploadDate);
+      }
       setSubmission(submissionResponse.data);
       setNonValidated(rawResponse.data.records
         .filter((each) => each.checked === false));
