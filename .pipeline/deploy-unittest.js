12db9109393a66976b3a539b1daedfb1b03b69fe
Kuan Fan <kuan.fan@gov.bc.ca>
2020-09-21 15:49:19 -0700

    update deploy

--- .pipeline/deploy-unittest.js
+++ .pipeline/deploy-unittest.js
@@ -1,5 +1,5 @@
 'use strict';
 const settings = require('./lib/config.js')
-const task = require('./lib/deploy.js')
+const task = require('./lib/deploy-unittest.js')
 
 task(Object.assign(settings, { phase: settings.options.env}));
