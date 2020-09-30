c8ab705d50f2ba2dd6ca6de074a762d16e912c7a
Kuan Fan <kuan.fan@gov.bc.ca>
2020-09-30 13:40:20 -0700

    update patroni monitor

--- openshift/templates/nagios/nagios3/commands/check_patroni_health.sh
+++ openshift/templates/nagios/nagios3/commands/check_patroni_health.sh
@@ -4,7 +4,7 @@ PATRONI_SERVICE_NAME=$1
 NAMESPACE=$2
 
 ## the value must be running
-master_node_state=`curl --silent --show-error http://$PATRONI_SERVICE_NAME.$NAMESPACE.local:8008/patroni | jq --raw-output .state`
+master_node_state=`curl --silent --show-error http://$PATRONI_SERVICE_NAME.$NAMESPACE.svc.cluster.local:8008/patroni | jq --raw-output .state`
 if [ $master_node_state != "running" ]; then
     exit 2
 fi
