d4ae45ec35cad3882c6e554265e07fe0c61990d0
Richard Tan <richard.tan@gov.bc.ca>
2020-10-07 08:56:51 -0700

    Reverted keycloak.py

--- backend/zeva/keycloak.py
+++ backend/zeva/keycloak.py
@@ -17,7 +17,7 @@ def config():
             'true'
         ).lower() in ['true', '1']),
         'SERVICE_ACCOUNT_REALM': os.getenv('KEYCLOAK_SA_REALM', 'zeva'),
-        'SERVICE_ACCOUNT_CLIENT_ID': os.getenv('KEYCLOAK_SA_CLIENT_ID', 'zeva-app-sa'),
+        'SERVICE_ACCOUNT_CLIENT_ID': os.getenv('KEYCLOAK_SA_CLIENT_ID', 'zeva'),
         'SERVICE_ACCOUNT_KEYCLOAK_API_BASE': os.getenv(
             'KEYCLOAK_SA_BASEURL',
             'http://localhost:8888'
