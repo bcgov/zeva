26c3c66cc4f2b292fa35e28c7dc80e61ab831873
Kuan Fan <kuan.fan@gov.bc.ca>
2020-10-21 15:31:23 -0700

    Migrate to v4

--- .jenkins/.pipeline/lib/config.js
+++ .jenkins/.pipeline/lib/config.js
@@ -6,9 +6,9 @@ const name = 'jenkins'
 const ocpName = 'pathfinder'
 
 const phases = {
-  build: {namespace:'tbiwaq-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`, ocpName: `${ocpName}`},
-  dev: {namespace:'tbiwaq-tools'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`, ocpName: `${ocpName}`},
-  prod: {namespace:'tbiwaq-tools'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`, ocpName: `${ocpName}`},
+  build: {namespace:'e52f12-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`, ocpName: `${ocpName}`},
+  dev: {namespace:'e52f12-tools'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`, ocpName: `${ocpName}`},
+  prod: {namespace:'e52f12-tools'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`, ocpName: `${ocpName}`},
 };
 
 // This callback forces the node process to exit as failure.
--- openshift/templates/jenkins/README.md
+++ openshift/templates/jenkins/README.md
@@ -1,11 +1,17 @@
 ### Build Jenkins base image
 
+for pathfinder: 
+    NAMESPACE_PREFIX=tbiwaq
+
+for Openshift v4: 
+    NAMESPACE_PREFIX=e52f12
+
 * Create the base image used by jenkins-bc.yaml
-oc tag registry.access.redhat.com/rhel7-atomic:7.7-371 tbiwaq-tools/rhel7-atomic:7.7-371
+oc tag registry.access.redhat.com/rhel7-atomic:7.7-371 e52f12-tools/rhel7-atomic:7.7-371
 
 * Build Jenkins base image to tools project
-oc process -f openshift/templates/jenkins/jenkins-bc.yaml | oc apply -f - -n tbiwaq-tools --dry-run=true
-Note: the base image is stored at tbiwaq-tools/bcgov-jenkins-basic:v2-20200303, update .jenkins/.pipeline/lib/build.js to use this image
+oc process -f openshift/templates/jenkins/jenkins-bc.yaml | oc apply -f - -n e52f12-tools --dry-run=client
+Note: the base image is stored at tbiwaq-tools/bcgov-jenkins-basic:v2-20201021, update .jenkins/.pipeline/lib/build.js to use this image
 
 * Create template.jenkins-github and template.jenkins-slave-user under tools project
 oc process -f .jenkins/openshift/deplpy-prereq.yaml GH_USERNAME=*** GH_ACCESS_TOKEN=***
