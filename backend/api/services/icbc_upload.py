1281bdf76a9ee7fd21f49ba5f541dc2f4e7f6bf7
Richard Tan <richard.tan@gov.bc.ca>
2020-11-02 15:39:26 -0800

    Tried chunking the dataframe

--- backend/api/services/icbc_upload.py
+++ backend/api/services/icbc_upload.py
@@ -18,66 +18,67 @@ def ingest_icbc_spreadsheet(excelfile, requesting_user, dateCurrentTo):
     with open("log.txt", "w") as outfile:
         outfile.write('starting')
 
-    df = pd.read_csv(excelfile, sep="|", error_bad_lines=False)
-    df.drop(df[(df.HYBRID_VEHICLE_FLAG == 'N') &
-            (df.ELECTRIC_VEHICLE_FLAG == 'N')].index, inplace=True)
-    df['MODEL_YEAR'].fillna(0, inplace=True)
-    df['VIN'].fillna(0, inplace=True)
-    df.drop(df[(df.MODEL_YEAR < 2019)].index, inplace=True)
-    df.drop(df[(df.VIN == 0)].index, inplace=True)
-    df.drop(df.columns.difference([
-        'VIN',
-        'MODEL',
-        'MODEL_YEAR',
-        'MAKE']), 1, inplace=True)
-    df = trim_all_columns(df)
-    df["MODEL"] = df["MODEL"].str.upper()
-    df["MAKE"] = df["MAKE"].str.upper()
-    df["MODEL_YEAR"] = df["MODEL_YEAR"].astype(int)
+    chunks = pd.read_csv(excelfile, sep="|", error_bad_lines=False, iterator=True, chunksize=1000)
+    for df in chunks:
+        df.drop(df[(df.HYBRID_VEHICLE_FLAG == 'N') &
+                (df.ELECTRIC_VEHICLE_FLAG == 'N')].index, inplace=True)
+        df['MODEL_YEAR'].fillna(0, inplace=True)
+        df['VIN'].fillna(0, inplace=True)
+        df.drop(df[(df.MODEL_YEAR < 2019)].index, inplace=True)
+        df.drop(df[(df.VIN == 0)].index, inplace=True)
+        df.drop(df.columns.difference([
+            'VIN',
+            'MODEL',
+            'MODEL_YEAR',
+            'MAKE']), 1, inplace=True)
+        df = trim_all_columns(df)
+        df["MODEL"] = df["MODEL"].str.upper()
+        df["MAKE"] = df["MAKE"].str.upper()
+        df["MODEL_YEAR"] = df["MODEL_YEAR"].astype(int)
 
-    # pd.options.display.float_format = '{:.0f}'.format
-    try:
-        # insert entry into the icbc upload date table
-        current_to_date = IcbcUploadDate.objects.create(
-            upload_date=dateCurrentTo,
-            create_user=requesting_user.username,
-            update_user=requesting_user.username,
-            )
+        # pd.options.display.float_format = '{:.0f}'.format
+        try:
+            # insert entry into the icbc upload date table
+            current_to_date = IcbcUploadDate.objects.create(
+                upload_date=dateCurrentTo,
+                create_user=requesting_user.username,
+                update_user=requesting_user.username,
+                )
 
-        # iterate through df and check if vehicle exists, if it doesn't, add it!
-        for index, row in df.iterrows():
-            with open("log.txt", "a") as outfile:
-                outfile.write(index)
-            icbc_vehicle_model = row['MODEL']
-            icbc_vehicle_year = row['MODEL_YEAR']
-            icbc_vehicle_make = row['MAKE']
-            icbc_vehicle_vin = row['VIN']
+            # iterate through df and check if vehicle exists, if it doesn't, add it!
+            for index, row in df.iterrows():
+                with open("log.txt", "a") as outfile:
+                    outfile.write(index)
+                icbc_vehicle_model = row['MODEL']
+                icbc_vehicle_year = row['MODEL_YEAR']
+                icbc_vehicle_make = row['MAKE']
+                icbc_vehicle_vin = row['VIN']
 
-            (model_year, _) = ModelYear.objects.get_or_create(
-                name=icbc_vehicle_year,
-                defaults={
-                    'create_user': requesting_user.username,
-                    'update_user': requesting_user.username
-                })
-            icbc_vehicle_year_id = model_year.id
-
-            (vehicle, _) = IcbcVehicle.objects.get_or_create(
-                    model_name=icbc_vehicle_model,
-                    model_year_id=icbc_vehicle_year_id,
-                    make=icbc_vehicle_make,
+                (model_year, _) = ModelYear.objects.get_or_create(
+                    name=icbc_vehicle_year,
                     defaults={
                         'create_user': requesting_user.username,
                         'update_user': requesting_user.username
                     })
-            vehicle_id = vehicle.id
+                icbc_vehicle_year_id = model_year.id
 
-            IcbcRegistrationData.objects.get_or_create(
-                vin=icbc_vehicle_vin,
-                defaults={
-                    'create_user': requesting_user.username,
-                    'update_user': requesting_user.username,
-                    'icbc_vehicle_id': vehicle_id,
-                    'icbc_upload_date_id': current_to_date.id
-                })
-    except Exception as e:
-        print(e)
+                (vehicle, _) = IcbcVehicle.objects.get_or_create(
+                        model_name=icbc_vehicle_model,
+                        model_year_id=icbc_vehicle_year_id,
+                        make=icbc_vehicle_make,
+                        defaults={
+                            'create_user': requesting_user.username,
+                            'update_user': requesting_user.username
+                        })
+                vehicle_id = vehicle.id
+
+                IcbcRegistrationData.objects.get_or_create(
+                    vin=icbc_vehicle_vin,
+                    defaults={
+                        'create_user': requesting_user.username,
+                        'update_user': requesting_user.username,
+                        'icbc_vehicle_id': vehicle_id,
+                        'icbc_upload_date_id': current_to_date.id
+                    })
+        except Exception as e:
+            print(e)
