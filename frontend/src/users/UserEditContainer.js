4606b3a71eb5df9ad32e7b9037a3ea8be64e6c22
emi-hi <emily.hillier@gmail.com>
2020-11-13 13:17:14 -0800

    removed console log

--- frontend/src/users/UserEditContainer.js
+++ frontend/src/users/UserEditContainer.js
@@ -93,7 +93,6 @@ const UserEditContainer = (props) => {
 
     const rolesPromise = axios.get(ROUTES_ROLES.LIST).then((response) => {
       setRoles(response.data);
-      console.log(response.data)
     });
 
     let detailsPromise;
