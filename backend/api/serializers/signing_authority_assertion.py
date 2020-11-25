0541c3b28031b623bcd1814dc4ab581db2af2c95
Richard <Richard.Tan@gov.bc.ca>
2020-11-25 08:41:30 -0800

    Zeva 329: Signing Confirmations (#407)
    * update to v1.14.0
    
    * Changed upload technique for icbc to do chunk uploads (#381)
    
    * update version to be v1.13.0
    
    * ZEVA-304: Credits in the Organizations Table (#363)
    
    * Added credits to the organization table. Some minor changes
    
    * Removed commented out css
    
    * Removed console.log
    
    * Updated error codes and highlighting of rows (#364)
    
    * ZEVA 192 - credit transfer other supplier accepts/rejects (#366)
    
    * refactored button story
    
    * set up front end to resemble transfer mockup with comment box and checkboxes
    
    * set up backend to post new validation status of transfer to database
    
    * added model for comments
    
    * added comment table, updated frontend so comments can now be posted
    
    * added conditions to render checkboxes, comments, and action bar
    
    * fixed styling on table, added condition to only show checkboxes if status is submitted, fixed prop type error for submission
    
    * wrote tests for transfer details page
    
    * replaced text -government- with  Government of B.C. (#367)
    
    * added 2021 fixture (#368)
    
    * edited save function and form fields so that only credit seller can initiate transfer (#369)
    
    * ZEVA 292: Update Roles and Permissions (#370)
    
    * Updated roles and permissions
    
    * Fixed fixture name conflict
    
    * Added back with state saving to the vehicle supplier pages (#373)
    
    * ZEVA 307 - add optional company/attorney (#371)
    
    * added representative name to database in organization address, added field to form on frontent
    
    * styled organization edit form
    
    * Added check for the validate and reject functions to show up (#374)
    
    * Changed upload technique for icbc to do chunk uploads
    
    * Added error catching
    
    * Removing the autodelete functionality for now
    
    * Increased timeout for gunicorn
    
    * More debugging
    
    * More debugging
    
    * Further increased timeout
    
    * Increased timeout to 10 mins
    
    * Removing the delete function for now:
    
    * Adding loggin
    
    * Moving the logging up
    
    * Fixed indentation
    
    * Changing the test logs
    
    * Tried chunking the dataframe
    
    * Optimizing the read_csv
    
    * reworked the uploader and parser
    
    * Moved the save date to before the processing of the spreadsheet
    
    * Turning off autocommit for the ingest function
    
    * Removing transaction auto commit false
    
    * Change to using transaction atomic
    
    * Testing without transactions
    
    * Set autocommit back. Temporarily put back the icbc date loop
    
    * Changed the date creation to get_or_create so it doesnt create too many records
    
    * Changed so that insert goes at first, and we just do a get after
    
    * Code cleanup
    
    * Trying out a higher chunk size for uploading
    
    Co-authored-by: Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
    Co-authored-by: Emily <44536222+emi-hi@users.noreply.github.com>
    
    * Signing Assertion models
    
    * Work in progress
    
    * Work in progress
    
    * Checkbox functions
    
    * Should now be saving the confirmations from the users
    
    Co-authored-by: Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
    Co-authored-by: Emily <44536222+emi-hi@users.noreply.github.com>

---
+++ backend/api/serializers/signing_authority_assertion.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from api.models.signing_authority_assertion import SigningAuthorityAssertion
+
+
+class SigningAuthorityAssertionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SigningAuthorityAssertion
+        fields = (
+            'id', 'description', 'effective_date', 'expiration_date',
+            'display_order'
+        )
