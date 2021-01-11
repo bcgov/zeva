fb59467ff368680434012a420f38dadbdbed2bfa
NavpreetGrewal <ngrewal002@gmail.com>
2021-01-11 14:35:16 -0800

    Notification fix

--- backend/api/viewsets/credit_transfer.py
+++ backend/api/viewsets/credit_transfer.py
@@ -13,10 +13,11 @@ from api.models.user_profile import UserProfile
 from api.models.notification import Notification
 from api.models.notification_subscription import NotificationSubscription
 from auditable.views import AuditableMixin
-from api.services.send_email import send_email
+from api.services.send_email import notifications_credit_transfers
 from api.services.credit_transaction import validate_transfer
 
 LOGGER = logging.getLogger(__name__)
+
 
 class CreditTransferViewset(
         AuditableMixin, viewsets.GenericViewSet,
@@ -91,41 +92,4 @@ class CreditTransferViewset(
     def perform_update(self, serializer, *args, **kwargs):
 
         transfer = serializer.save()
-        email = None
-        if transfer.status == CreditTransferStatuses.VALIDATED:
-            #call service that updates transactions and balance tables
-            validate_transfer(transfer)
-            notifications = Notification.objects.values_list('id', flat=True).filter(Q(notification_code='CREDIT_TRANSFER_RECORDED_GOVT') | Q(notification_code='CREDIT_TRANSFER_RECORDED'))
-    
-        elif transfer.status == CreditTransferStatuses.RECOMMEND_APPROVAL:
-            notifications = Notification.objects.values_list('id', flat=True).filter(notification_code='CREDIT_TRANSFER_RECOMMEND_APPROVAL') 
-
-        elif transfer.status == CreditTransferStatuses.RECOMMEND_REJECTION:
-            notifications = Notification.objects.values_list('id', flat=True).filter(notification_code='CREDIT_TRANSFER_RECOMMEND_REJECT') 
-               
-        elif transfer.status == CreditTransferStatuses.APPROVED:
-            notifications = Notification.objects.values_list('id', flat=True).filter(Q(notification_code='CREDIT_TRANSFER_APPROVED') | Q(notification_code='CREDIT_TRANSFER_APPROVED_PARTNER'))
-
-        elif transfer.status == CreditTransferStatuses.DISAPPROVED:
-            notifications = Notification.objects.values_list('id', flat=True).filter(notification_code='CREDIT_TRANSFER_REJECT_PARTNER')
-
-        elif transfer.status == CreditTransferStatuses.RESCINDED:
-            notifications = Notification.objects.values_list('id', flat=True).filter(Q(notification_code='CREDIT_TRANSFER_RESCIND') | Q(notification_code='CREDIT_TRANSFER_RESCIND_PARTNER'))
-
-        elif transfer.status == CreditTransferStatuses.RESCIND_PRE_APPROVAL:
-            notifications = Notification.objects.values_list('id', flat=True).filter(notification_code='CREDIT_TRANSFER_RESCIND_PARTNER') 
-
-        elif transfer.status == CreditTransferStatuses.REJECTED:
-            notifications = Notification.objects.values_list('id', flat=True).filter(Q(notification_code='CREDIT_TRANSFER_REJECTED_GOVT') | Q(notification_code='CREDIT_TRANSFER_REJECTED'))
-        """
-        Send email to the users based on their notification subscription for a credit transfer
-        """   
-        try:
-            subscribed_users = NotificationSubscription.objects.values_list('user_profile_id', flat=True).filter(notification__id__in=notifications)
-            if subscribed_users:
-                user_email = UserProfile.objects.values_list('email', flat=True).filter(id__in=subscribed_users).exclude(email__isnull=True).exclude(email__exact='')
-                if user_email:
-                    send_email(list(user_email))            
-        except Exception as e:
-            LOGGER.error('Email Failed! %s', e)  
-
+        notifications_credit_transfers(transfer)
