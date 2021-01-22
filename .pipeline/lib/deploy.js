99a88dd3c6d9226b37523e8ce6364dce476f0d1e
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2021-01-22 09:48:36 -0800

    Update frontend configmap and backend replica (#524)
    * update frontend configs
    * update test and prod backend replica

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -148,15 +148,20 @@ module.exports = settings => {
   */
 
   // deploy frontend configmap
-  if(phase === 'dev') {
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-configmap.yaml`, {
-      'param': {
-        'NAME': phases[phase].name,
-        'SUFFIX': phases[phase].suffix,
-        'CREDIT_TRANSFER_ENABLED': phases[phase].creditTransferEnabled
-      }
-    }))
-  }
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-configmap.yaml`, {
+    'param': {
+      'NAME': phases[phase].name,
+      'SUFFIX': phases[phase].suffix,
+      'CREDIT_TRANSFERS_ENABLED': phases[phase].creditTransfersEnabled,
+      'CREDIT_TRANSACTIONS_ENABLED': phases[phase].creditTransactionsEnabled,
+      'INITIATIVE_AGREEMENTS_ENABLED': phases[phase].initiativeAgreementsEnabled,
+      'MODEL_YEAR_REPORT_ENABLED': phases[phase].modelYearReportEnabled,
+      'PURCHASE_REQUESTS_ENABLED': phases[phase].purchaseRequestsEnabled,
+      'NOTIFICATIONS_ENABLED': phases[phase].notificationsEnabled,
+      'COMPLIANCE_REPORT_ENABLED': phases[phase].complianceReportEnabled,
+      'ROLES_ENABLED': phases[phase].rolesEnabled
+    }
+  }))
 
   // deploy frontend
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-dc.yaml`, {
