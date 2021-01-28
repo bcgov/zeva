bfbc6d3a4b61ef97b819dd60038ad17280554119
Richard Tan <richard.tan@gov.bc.ca>
2021-01-28 14:28:20 -0800

    Removed test code

--- frontend/src/compliance/ComplianceReportsContainer.js
+++ frontend/src/compliance/ComplianceReportsContainer.js
@@ -4,14 +4,12 @@ import PropTypes from 'prop-types';
 import CustomPropTypes from '../app/utilities/props';
 import ComplianceTabs from '../app/components/ComplianceTabs';
 import ComplianceReportsDetailsPage from './components/ComplianceReportsDetailsPage';
-import ComplianceReportTabs from './components/ComplianceReportTabs';
 
 const ComplianceReportsContainer = (props) => {
   const { user } = props;
   return (
     <>
       <ComplianceTabs active="reports" user={user} />
-      <ComplianceReportTabs />
       <ComplianceReportsDetailsPage user={user} />
     </>
   );
