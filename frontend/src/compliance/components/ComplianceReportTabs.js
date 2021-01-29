42416b7d19b0eddac4dac83f6a10954d68938787
Richard <Richard.Tan@gov.bc.ca>
2021-01-29 08:27:33 -0800

    ZEVA-438: Compliance Report Tabs (#536)
    * Created compliance report tabs
    
    * Removed test code

---
+++ frontend/src/compliance/components/ComplianceReportTabs.js
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import ROUTES_COMPLIANCE from '../../app/routes/Compliance';
+
+const ComplianceReportTabs = (props) => {
+  const { active, reportStatuses } = props;
+
+  return (
+    <ul
+      className="nav nav-pills nav-fill compliance-report-tabs"
+      key="tabs"
+      role="tablist"
+    >
+      <li
+        className={`nav-item ${(active === 'supplier-information') ? 'active' : ''} ${reportStatuses.supplierInformation}`}
+        role="presentation"
+      >
+        <Link to={ROUTES_COMPLIANCE.REPORT_SUPPLIER_INFORMATION}>Supplier Information</Link>
+      </li>
+      <li
+        className={`nav-item ${(active === 'consumer-sales') ? 'active' : ''} ${reportStatuses.consumerSales}`}
+        role="presentation"
+      >
+        <Link to={ROUTES_COMPLIANCE.REPORT_CONSUMER_SALES}>Consumer Sales</Link>
+      </li>
+      <li
+        className={`nav-item ${(active === 'credit-activity') ? 'active' : ''} ${reportStatuses.creditActivity}`}
+        role="presentation"
+      >
+        <Link to={ROUTES_COMPLIANCE.REPORT_CREDIT_ACTIVITY}>Credit Activity</Link>
+      </li>
+      <li
+        className={`nav-item ${(active === 'summary') ? 'active' : ''} ${reportStatuses.reportSummary}`}
+        role="presentation"
+      >
+        <Link to={ROUTES_COMPLIANCE.REPORT_SUMMARY}>Summary</Link>
+      </li>
+      <li
+        className={`nav-item ${(active === 'assessment') ? 'active' : ''} ${reportStatuses.assessment}`}
+        role="presentation"
+      >
+        <Link to={ROUTES_COMPLIANCE.REPORT_ASSESSMENT}>Assessment</Link>
+      </li>
+    </ul>
+  );
+};
+
+ComplianceReportTabs.defaultProps = {
+  reportStatuses: {
+    assessment: '',
+    consumerSales: '',
+    creditActivity: '',
+    reportSummary: '',
+    supplierInformation: '',
+  },
+};
+
+ComplianceReportTabs.propTypes = {
+  active: PropTypes.string.isRequired,
+  reportStatuses: PropTypes.shape(),
+};
+
+export default ComplianceReportTabs;
