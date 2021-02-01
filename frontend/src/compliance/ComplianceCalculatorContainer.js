0ef12a668b272e7d31644bd2512698cdb621f23e
emi-hi <emily.hillier@gmail.com>
2021-02-01 09:22:26 -0800

    adds refresh details with space for axios calls for years and ratios

--- frontend/src/compliance/ComplianceCalculatorContainer.js
+++ frontend/src/compliance/ComplianceCalculatorContainer.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Loading from '../app/components/Loading';
 import CustomPropTypes from '../app/utilities/props';
 import ComplianceTabs from '../app/components/ComplianceTabs';
 import ComplianceCalculatorDetailsPage from './components/ComplianceCalculatorDetailsPage';
@@ -7,6 +9,23 @@ const LDVSalesContainer = (props) => {
   const { user } = props;
   const [supplierSize, setSupplierSize] = useState('');
   const [modelYear, setModelYear] = useState({});
+  const [complianceRatios, setComplianceRatios] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const refreshDetails = () => {
+    axios.all([
+      // axios.get(ROUTE FOR MODEL YEARS?),
+      // axios.get(ROUTE FOR COMPLIANCE RATIOS),
+    ]).then(axios.spread((modelYearResponse, complianceRatiosResponse) => {
+      setModelYear(modelYearResponse);
+      setComplianceRatios(complianceRatiosResponse);
+      setLoading(false);
+    }));
+  };
+
+  useEffect(() => {
+    refreshDetails();
+  }, []);
 
   return (
     <>
