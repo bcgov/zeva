b47791375592bb68a1d52801a415eaf9f4714b4e
NavpreetGrewal <ngrewal002@gmail.com>
2021-02-03 10:52:15 -0800

    Changed float field to decimal to add db_comments

--- backend/api/models/compliance_ratio.py
+++ backend/api/models/compliance_ratio.py
@@ -15,8 +15,18 @@ class ComplianceRatio(Auditable):
         max_length=250,
         db_comment='model year'
     )
-    compliance_ratio = models.FloatField()
-    zev_class_a = models.FloatField()
+    compliance_ratio = models.DecimalField(
+        blank=False,
+        decimal_places=2,
+        max_digits=20,
+        db_comment="Containes the compliance ratio percentage for for each model year for light-duty motor vehicle class."
+    )
+    zev_class_a = models.DecimalField(
+        blank=False,
+        decimal_places=2,
+        max_digits=20,
+        db_comment="Contains the compliance ratio percentage for ZEV Class A vehicles in the light-duty motor vehicle class."
+    )
 
     class Meta:
         db_table = "compliance_ratio"
