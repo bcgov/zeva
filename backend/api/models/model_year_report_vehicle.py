254ae50e317cf896439d54156c336817aaa4c955
NavpreetGrewal <ngrewal002@gmail.com>
2021-02-10 15:34:54 -0800

    ZEVA-454:Create database tables to support backend for consumer sales

---
+++ backend/api/models/model_year_report_vehicle.py
@@ -0,0 +1,59 @@
+"""
+Model Year Report Vehicle
+"""
+from django.db import models
+
+from auditable.models import Auditable
+
+
+class ModelYearReportVehicle(Auditable):
+    """
+    Table to store ZEV vehicle information based on model year
+    """
+    pending_sales = models.IntegerField(
+        db_comment="Sum of pending sales for zev models"
+    )
+    sales_issued = models.IntegerField(
+        db_comment="Sum of issued sales for zev models"
+    )
+    model_year = models.ForeignKey(
+        'ModelYear',
+        related_name=None,
+        on_delete=models.PROTECT,
+        null=False
+    )
+    make = models.CharField(
+        blank=False,
+        db_comment="Make of vehicle",
+        max_length=250,
+        null=False
+    )
+    model = models.CharField(
+        blank=False,
+        db_comment="Model of vehicle",
+        max_length=250,
+        null=False
+    )
+    zev_type = models.ForeignKey(
+        'ZevType',
+        related_name=None,
+        on_delete=models.PROTECT,
+        null=False
+    )
+    range = models.DecimalField(
+        blank=False,
+        decimal_places=2,
+        max_digits=20,
+        db_comment="vehicle range in km"
+    )
+    zev_class = models.ForeignKey(
+        'VehicleClass',
+        related_name=None,
+        on_delete=models.PROTECT,
+        null=False
+    )
+
+    class Meta:
+        db_table = "model_year_report_vehicle"
+
+    db_table_comment = "Table to store ZEV vehicle information based on model year"
