239395c1d08aa8f20da01a70f77ef227679caa29
Richard <Richard.Tan@gov.bc.ca>
2021-02-11 14:53:29 -0800

    Added model year report address (#569)
    Co-authored-by: NavpreetGrewal <65915811+NavpreetGrewal@users.noreply.github.com>

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -33,6 +33,6 @@ from . import sales_evidence
 from . import compliance_ratio
 from . import model_year_report, model_year_report_confirmation
 from . import model_year_report_history, model_year_report_make
-from . import model_year_report_statuses
+from . import model_year_report_statuses, model_year_report_address
 from . import model_year_report_previous_sales
 from . import model_year_report_vehicle
