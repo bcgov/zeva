a6e1a2323501bd5b7d103be5b2dcddbf49026a2b
emi-hi <emily.hillier@gmail.com>
2021-02-12 15:52:34 -0800

    -makes page more responsive, fixes css -switches table to using <table> rather than divs

--- frontend/src/compliance/components/ComplianceCalculatorDetailsPage.js
+++ frontend/src/compliance/components/ComplianceCalculatorDetailsPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CustomPropTypes from '../../app/utilities/props';
+import ComplianceCalculatorDetailsTotals from './ComplianceCalculatorDetailsTotals';
+import ComplianceCalculatorModelTable from './ComplianceCalculatorModelTable';
+import ComplianceCalculatorDetailsInputs from './ComplianceCalculatorDetailsInputs';
 
 const ComplianceCalculatorDetailsPage = (props) => {
   const {
@@ -10,6 +13,10 @@ const ComplianceCalculatorDetailsPage = (props) => {
     modelYearList,
     selectedYearOption,
     supplierSize,
+    allVehicleModels,
+    estimatedModelSales,
+    setEstimatedModelSales,
+    user,
   } = props;
   const selectionList = modelYearList.map((obj) => (
     <option key={(obj.id)} value={obj.name}>{obj.name}</option>
@@ -21,141 +28,27 @@ const ComplianceCalculatorDetailsPage = (props) => {
           <h2>Credit Obligation Calculator</h2>
         </div>
       </div>
-      <div id="form">
-        <div className="row">
-          <div className="col-lg-12 col-xl-10">
-            <fieldset>
-              <div className="form-group row mb-0">
-                <label
-                  className="col-sm-4 col-form-label"
-                  htmlFor="model-year"
-                >
-                  Model Year:
-                </label>
-                <div className="col-sm-2  pl-0">
-                  <select
-                    className="form-control"
-                    id="model-year"
-                    name="model-year"
-                    value={selectedYearOption}
-                    onChange={(event) => { handleInputChange(event); }}
-                  >
-                    {selectedYearOption === '--' && <option disabled>--</option>}
-                    {selectionList}
-                  </select>
-                </div>
-              </div>
-              <div className="form-group mt-0">
-                <div className="text-blue">
-                  <label
-                    htmlFor="supplier-size"
-                    className="pl-3 col-form-label"
-                  >
-                    Supplier Size
-                  </label>
-                  &nbsp; (based on average of previous 3 year total LDV sale):
-                </div>
-                <div className="pl-5">
-                  <span className="text-blue ml-4">Small Volume Supplier - no obligation (under 1,000 total LDV sales)</span>
-                  <br />
-                  <input
-                    type="radio"
-                    id="supplier-size"
-                    name="supplier-size"
-                    value="medium"
-                    onChange={(event) => { handleInputChange(event); }}
-                  />
-                  <span className="text-blue">Medium Volume Supplier (under 5,000 total LDV sales)</span>
-                  <br />
-                  <input
-                    type="radio"
-                    id="supplier-size"
-                    name="supplier-size"
-                    value="large"
-                    onChange={(event) => { handleInputChange(event); }}
-                  />
-                  <span className="text-blue">Large Volume Supplier (under 10,000 total LDV sales)</span>
-                </div>
-                <div className="form-group row mb-0">
-                  <div className="col-sm-5 text-blue pl-0">
-                    Compliance Ratio:
-
-                  </div>
-                  {supplierSize && (selectedYearOption !== '--') && (
-                    <div className="col-sm-6">
-                      {complianceYearInfo.complianceRatio}%
-                    </div>
-                  )}
-                  {supplierSize === 'large'
-                    && (selectedYearOption !== '--') && (
-                    <>
-                      <div className="col-sm-5 text-blue pl-0">
-                        Large Supplier Class A Ratio:
-
-                      </div>
-                      <div className="col-sm-6">
-                        {complianceYearInfo.zevClassA}%
-
-                      </div>
-                    </>
-                  )}
-                </div>
-                <div className="form-group row mb-0 py-0">
-                  <label
-                    className="col-sm-4 col-form-label pl-0"
-                    htmlFor="provisional-sales"
-                  >
-                    Provisional LDV Sales:
-                  </label>
-                  <input
-                    type="number"
-                    min="0"
-                    step="1"
-                    id="total-sales-number"
-                    className="col-sm-2"
-                    onChange={(event) => { handleInputChange(event); }}
-                  />
-                </div>
-                <div className="form-group row mb-0 py-0">
-                  <label
-                    className="col-sm-5 col-form-label pl-0 pb-0"
-                    htmlFor="provisional-sales"
-                  >
-                    Provisional Credit Target Total:
-                  </label>
-                  <div className="col-sm-6 pb-0">
-                    <b>
-                      {complianceNumbers.total}
-                    </b>
-                  </div>
-                  <label
-                    className="col-sm-5 col-form-label pl-0 pb-0"
-                    htmlFor="provisional-sales"
-                  >
-                    Provisional Class A Credit Obligation:
-                  </label>
-                  <div className="col-sm-6 pb-0">
-                    <b>
-                      {complianceNumbers.classA}
-                    </b>
-                  </div>
-                  <label
-                    className="col-sm-5 col-form-label pl-0 pb-0"
-                    htmlFor="provisional-sales"
-                  >
-                    Remaining Class A or B Credit Obligation:
-                  </label>
-                  <div className="col-sm-6">
-                    <b>
-                      {complianceNumbers.remaining}
-                    </b>
-                  </div>
-                </div>
-              </div>
-
-            </fieldset>
-          </div>
+      <div className="calculator-page">
+        <div id="form" className="row px-2">
+          <ComplianceCalculatorDetailsInputs
+            complianceYearInfo={complianceYearInfo}
+            handleInputChange={handleInputChange}
+            modelYearList={modelYearList}
+            selectedYearOption={selectedYearOption}
+            supplierSize={supplierSize}
+          />
+          <ComplianceCalculatorDetailsTotals
+            user={user}
+            complianceNumbers={complianceNumbers}
+            estimatedModelSales={estimatedModelSales}
+            supplierSize={supplierSize}
+          />
         </div>
+        <ComplianceCalculatorModelTable
+          models={allVehicleModels}
+          estimatedModelSales={estimatedModelSales}
+          setEstimatedModelSales={setEstimatedModelSales}
+        />
       </div>
     </div>
   );
@@ -163,6 +56,7 @@ const ComplianceCalculatorDetailsPage = (props) => {
 ComplianceCalculatorDetailsPage.defaultProps = {
   supplierSize: '',
   selectedYearOption: '--',
+  complianceNumbers: { total: '', classA: '', remaining: '' },
 };
 ComplianceCalculatorDetailsPage.propTypes = {
   complianceNumbers: PropTypes.shape({
@@ -178,15 +72,18 @@ ComplianceCalculatorDetailsPage.propTypes = {
       PropTypes.number,
       PropTypes.string,
     ]),
-  }).isRequired,
+  }),
   complianceYearInfo: PropTypes.shape({
-    complianceRatio: PropTypes.number,
-    zevClassA: PropTypes.number,
+    complianceRatio: PropTypes.string,
+    zevClassA: PropTypes.string,
   }).isRequired,
   handleInputChange: PropTypes.func.isRequired,
   modelYearList: PropTypes.arrayOf(PropTypes.shape()).isRequired,
   selectedYearOption: PropTypes.string,
   supplierSize: PropTypes.string,
+  estimatedModelSales: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  setEstimatedModelSales: PropTypes.func.isRequired,
+  user: CustomPropTypes.user.isRequired,
 };
 
 export default ComplianceCalculatorDetailsPage;
---
+++ frontend/src/compliance/components/ComplianceCalculatorDetailsTotals.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CustomPropTypes from '../../app/utilities/props';
+import formatNumeric from '../../app/utilities/formatNumeric';
+
+const ComplianceCalculatorDetailsTotals = (props) => {
+  const { complianceNumbers, supplierSize, estimatedModelSales, user } = props;
+  return (
+    <div className="col-lg-4 col-12 px-3 compliance-calculator-totals m-lg-2">
+      <div className="form-group row p-3">
+        <table className="m-auto">
+          <tbody>
+            <tr>
+              <td className="font-weight-bold text-blue pt-4">
+                Provisional Compliance Ratio Reduction:
+              </td>
+              <td className="pl-3 font-weight-bold pt-4 text-right">
+                {complianceNumbers.total}
+              </td>
+            </tr>
+            {supplierSize === 'large'
+          && (
+          <>
+            <tr>
+              <td className="text-blue">
+                -ZEV Class A Debit:
+              </td>
+              <td className="pl-3 text-right">
+                {complianceNumbers.classA}
+              </td>
+            </tr>
+            <tr>
+              <td className="text-blue">
+                -Unspecified ZEV Class Debit:
+              </td>
+              <td className="pl-3 text-right">
+                {complianceNumbers.remaining}
+              </td>
+            </tr>
+          </>
+          )}
+            <tr>
+              <td className="font-weight-bold text-blue pt-4">
+                Current Credit Balance:
+              </td>
+              <td />
+            </tr>
+            <tr>
+              <td className="text-blue">
+                Class A Credit Total:
+              </td>
+              <td className="pl-3 text-right">{user.organization.balance.A}</td>
+            </tr>
+            <tr>
+              <td className="text-blue">
+                Class B Credit Total:
+              </td>
+              <td className="pl-3 text-right">{user.organization.balance.B}</td>
+
+            </tr>
+            <tr>
+              <td className="text-blue pt-4">
+                <b>Estimated Annual ZEV Sales</b> (from below):
+              </td>
+              <td />
+            </tr>
+            <tr>
+              <td className="text-blue">
+                Estimated Class A Credit Total:
+              </td>
+              <td className="pl-3 text-right">
+                {estimatedModelSales && formatNumeric(estimatedModelSales.filter((each) => each.creditClass === 'A').reduce((a, v) => a + v.value, 0))}
+
+              </td>
+            </tr>
+            <tr>
+              <td className="text-blue">
+                Estimated Class B Credit Total:
+              </td>
+              <td className="pl-3 text-right">
+                {estimatedModelSales && formatNumeric(estimatedModelSales.filter((each) => each.creditClass === 'B').reduce((a, v) => a + v.value, 0))}
+
+              </td>
+
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+ComplianceCalculatorDetailsTotals.defaultProps = {
+
+};
+ComplianceCalculatorDetailsTotals.propTypes = {
+
+};
+
+export default ComplianceCalculatorDetailsTotals;
