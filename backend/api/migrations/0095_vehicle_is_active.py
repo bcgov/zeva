d78891a2e3b31d81313e5b579fb2c5de810e4b5f
emi-hi <emily.hillier@gmail.com>
2021-02-19 15:06:03 -0800

    fixed migration conflicts

--- backend/api/migrations/0095_vehicle_is_active.py
+++ backend/api/migrations/0095_vehicle_is_active.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0093_modelyearreport_organization'),
+        ('api', '0094_auto_20210217_1552'),
     ]
 
     operations = [
