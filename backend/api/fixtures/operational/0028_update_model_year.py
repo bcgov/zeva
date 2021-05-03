ffef15574743cbc1960d03e1e0345c9fcfd51b76
Richard Tan <richard.tan@gov.bc.ca>
2021-05-03 13:15:29 -0700

    Added a check and updates the record instead of failing the year creation

--- backend/api/fixtures/operational/0028_update_model_year.py
+++ backend/api/fixtures/operational/0028_update_model_year.py
@@ -12,24 +12,24 @@ class UpdateModelYear(OperationalDataScript):
     comment = 'Add 2022-2023 model years'
 
     def check_run_preconditions(self):
-        to_check = ["2022,2023"]
-        for year in to_check:
-            if ModelYear.objects.filter(name=year).exists():
-                return False
         return True
 
     @transaction.atomic
     def run(self):
-        ModelYear.objects.create(
-            effective_date="2022-01-01",
-            expiration_date="2022-12-31",
-            name="2022"
+        ModelYear.objects.update_or_create(
+            name="2022",
+            defaults={
+                'effective_date': "2022-01-01",
+                'expiration_date': "2022-12-31"
+            }
         )
 
-        ModelYear.objects.create(
-            effective_date="2023-01-01",
-            expiration_date="2023-12-31",
-            name="2023"
+        ModelYear.objects.update_or_create(
+            name="2023",
+            defaults={
+                'effective_date': "2023-01-01",
+                'expiration_date': "2023-12-31"
+            }
         )
 
 
