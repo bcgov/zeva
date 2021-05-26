69631ede9621cc2eceadb2884bf9790199452dc1
emi-hi <emily.hillier@gmail.com>
2021-05-26 13:43:53 -0700

    -adds descriptions list serializer to backend -iterates descriptions on frontend

--- backend/api/models/model_year_report_assessment_descriptions.py
+++ backend/api/models/model_year_report_assessment_descriptions.py
@@ -2,11 +2,11 @@
 Model Year Assessment Comment Model
 """
 from django.db import models
-
+from api.models.mixins.display_order import DisplayOrder
 from auditable.models import Auditable
 
 
-class ModelYearReportAssessmentDescriptions(Auditable):
+class ModelYearReportAssessmentDescriptions(Auditable, DisplayOrder):
     """
     contains descriptions for the 3 options for the assessment
     radio buttons
@@ -22,7 +22,7 @@ class ModelYearReportAssessmentDescriptions(Auditable):
 
     class Meta:
         db_table = 'model_year_report_assessment_descriptions'
-        ordering = ['create_timestamp']
+        ordering = ['display_order']
 
     db_table_comment = \
         "a lookup table for descriptions of the assessment" \
