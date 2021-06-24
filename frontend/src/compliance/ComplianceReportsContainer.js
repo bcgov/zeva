6b43cc9cb45b68b62685f9e99fad6656b1a0e138
emi-hi <emily.hillier@gmail.com>
2021-06-24 15:33:48 -0700

    -fixes logic to exclude government users when deciding which reports could be created

--- frontend/src/compliance/ComplianceReportsContainer.js
+++ frontend/src/compliance/ComplianceReportsContainer.js
@@ -22,59 +22,63 @@ const ComplianceReportsContainer = (props) => {
     setLoading(showLoading);
     axios.get(ROUTES_COMPLIANCE.REPORTS).then((response) => {
       setData(response.data);
-      const allRecords = [];
-      response.data.forEach((record) => {
-        const status = record.validationStatus;
-        const year = record.modelYear.name;
-        allRecords.push({ year, status });
-      });
-      // sort records by year
-      allRecords.sort((a, b) => parseFloat(a.year) - parseFloat(b.year));
-      // if validation status exists but is not assessed for one year
-      // do not add any further years to filtered years/available years
-      let unfinishedFlag = false;
-      const completedYears = [];
-      allRecords.every((record) => {
-        if (record.status !== 'ASSESSED') {
-          unfinishedFlag = true;
-          return false;
-        }
-        completedYears.push(record.year);
-        return true;
-      });
-      let filteredYears = [];
-      const mostRecentCompleted = completedYears.pop();
-      const nextYear = parseInt(mostRecentCompleted, 10) + 1;
-      if (availableYears.lenth > 0) {
+      // if the user is a supplier, figure out which years to allow them to create reports for
+      if (!user.isGovernment) {
+        const allRecords = [];
+        response.data.forEach((record) => {
+          const status = record.validationStatus;
+          const year = record.modelYear.name;
+          allRecords.push({ year, status });
+        });
+        // sort records by year
+        allRecords.sort((a, b) => parseFloat(a.year) - parseFloat(b.year));
+        // if validation status exists but is not assessed for one year
+        // do not add any further years to filtered years/available years
+        let unfinishedFlag = false;
+        const completedYears = [];
+        allRecords.every((record) => {
+          if (record.status !== 'ASSESSED') {
+            unfinishedFlag = true;
+            return false;
+          }
+          completedYears.push(record.year);
+          return true;
+        });
+        let filteredYears = [];
+        const mostRecentCompleted = completedYears.pop();
+        const nextYear = parseInt(mostRecentCompleted, 10) + 1;
+        if (availableYears.length > 0) {
         // show only the next year if all previous reports have been assessed
-        filteredYears = availableYears.filter((year) => (
-          year === nextYear
-        ));
-      }
-      const { ldvSales } = user.organization;
-      if (ldvSales) {
-        // if an organization has ldv sales, only allow them to submit reports
-        // for the year after their most recent sale
-        ldvSales.sort((a, b) => parseFloat(a.modelYear) - parseFloat(b.modelYear));
-        const mostRecentSale = ldvSales.pop();
-        filteredYears = availableYears.filter((year) => (
-          year === parseInt(mostRecentSale.modelYear, 10) + 1
-        ));
-      } else {
-        // if an organization does not have ldv sales, only allow them to submit
-        // reports for the current year and previous year
-        const today = new Date();
-        const currentYear = today.getFullYear();
-        const previousYear = currentYear - 1;
-        filteredYears = availableYears.filter((year) => (
-          year === currentYear || year === previousYear
-        ));
-      }
+          filteredYears = availableYears.filter((year) => (
+            year === nextYear
+          ));
+        } else if (availableYears.length === 0) {
+          const { ldvSales } = user.organization;
+          if (ldvSales.length > 0) {
+            // if an organization has ldv sales, only allow them to submit reports
+            // for the year after their most recent sale
+            ldvSales.sort((a, b) => parseFloat(a.modelYear) - parseFloat(b.modelYear));
+            const mostRecentSale = ldvSales.pop();
+            filteredYears = availableYears.filter((year) => (
+              year === parseInt(mostRecentSale.modelYear, 10) + 1
+            ));
+          } else {
+            // if an organization does not have ldv sales, only allow them to submit
+            // reports for the current year and previous year
+            const today = new Date();
+            const currentYear = today.getFullYear();
+            const previousYear = currentYear - 1;
+            filteredYears = availableYears.filter((year) => (
+              year === currentYear || year === previousYear
+            ));
+          }
+        }
 
-      if (unfinishedFlag) {
-        filteredYears = [];
+        if (unfinishedFlag) {
+          filteredYears = [];
+        }
+        setAvailableYears(filteredYears);
       }
-      setAvailableYears(filteredYears);
       setLoading(false);
     });
   };
