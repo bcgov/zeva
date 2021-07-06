7beeb2148426a48ecc07332804631b2dfbf51674
Kuan Fan <kuan.fan@gov.bc.ca>
2021-07-06 16:06:54 -0700

    ZEVA-594 Credit Agreements

---
+++ frontend/src/credits/components/CreditAgreementsFilter.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const CreditAgreementsFilter = (props) => {
+    const {
+        items,
+        handleClear,
+        filtered,
+        setFiltered
+    } = props;
+
+    const getOptions = (inputObj, displayField) => {
+        const uniqueArr = [...new Set(inputObj.map((eachAgreement) => {
+          if (typeof eachAgreement[displayField] === 'string') {
+            return eachAgreement[displayField];
+          }
+    
+          return eachAgreement[displayField].shortName || eachAgreement[displayField].name;
+        }))];
+        return uniqueArr.sort().map((each) => (
+          <option key={each}>{each}</option>
+        ));
+      };
+    
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        console.log(id);
+        console.log(value);
+        let newFiltered = [...filtered];
+        console.log(newFiltered);
+        newFiltered = newFiltered.filter((each) => (each.id !== id));
+        setFiltered([...newFiltered, { id, value }]);
+    };      
+          
+    return (
+        <div className="action-bar p-2 justify-content-end action-bar-background">
+            <span className="right-content d-block d-md-flex d-lg-flex d-xl-flex">
+                <label className="my-0">Filter by supplier/status</label>
+                <select
+                    className="form-control"
+                    id="col-supplier"
+                    onChange={handleChange}
+                    value={filtered.length > 0 && filtered.findIndex((arr) => (arr.id === 'col-supplier')) >= 0 ? filtered[filtered.findIndex((arr) => (arr.id === 'col-supplier'))].value : ''}>
+                    <option value=""> </option>
+                    {getOptions(items, 'supplier')}
+                </select>
+                <select
+                    className="form-control"
+                    id="col-status"
+                    onChange={handleChange}
+                    value={filtered.length > 0 && filtered.findIndex((arr) => (arr.id === 'col-status')) >= 0 ? filtered[filtered.findIndex((arr) => (arr.id === 'col-status'))].value : ''}>
+                    <option value=""> </option>
+                    {getOptions(items, 'status')}
+                </select>
+                <button
+                    className="button"
+                    onClick={handleClear}
+                    type="button">
+                    Clear Filters
+                </button>
+                <button
+                  className="button primary"
+                  type="button">
+                  <FontAwesomeIcon icon="plus" /> New Transaction
+                </button>
+            </span>
+      </div>         
+    )
+} 
+
+CreditAgreementsFilter.propTypes = {
+    handleClear: PropTypes.func.isRequired,
+    filtered: PropTypes.arrayOf(PropTypes.object).isRequired,
+    setFiltered: PropTypes.func.isRequired,
+    items: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  };
+
+export default CreditAgreementsFilter;
+
---
+++ frontend/src/credits/components/CreditAgreementsListPage.js
@@ -0,0 +1,75 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Loading from '../../app/components/Loading';
+import history from '../../app/History';
+import CustomPropTypes from '../../app/utilities/props';
+import ROUTES_CREDIT_AGREEMENTS from '../../app/routes/CreditAgreements';
+import CreditAgreementsListTable from './CreditAgreementsListTable';
+import CreditAgreementsFilter from './CreditAgreementsFilter';
+
+const CreditAgreementsListPage = (props) => {
+  const {
+    creditAgreements,
+    filtered,
+    handleClear,
+    loading,
+    setFiltered,
+    user,
+  } = props;
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div id="CreditsAgreement" className="page">
+      <div className="row mt-3 mb-2">
+        <div className="col-md-8 d-flex align-items-end">
+          <h2>Credit Agreements & Adjustments</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12 text-right content">
+          <CreditAgreementsFilter 
+            items={creditAgreements}
+            filtered={filtered}
+            handleClear={handleClear}
+            setFiltered={setFiltered} />
+        </div>
+      </div>
+      <div className="row mt-3">
+        <label></label>
+      </div>
+      <div className="row">
+        <div className="col-md-12">
+          <CreditAgreementsListTable
+            items={creditAgreements}
+            user={user}
+            filtered={filtered}
+            setFiltered={setFiltered}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+CreditAgreementsListPage.defaultProps = {
+  filtered: undefined,
+  setFiltered: undefined,
+};
+
+CreditAgreementsListPage.propTypes = {
+  creditAgreements: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  filtered: PropTypes.arrayOf(PropTypes.shape()),
+  handleClear: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  setFiltered: PropTypes.func,
+  user: CustomPropTypes.user.isRequired,
+};
+
+
+export default CreditAgreementsListPage;
