ee5889a7b7ffaf66d5e5c113f2d2cd8f71d34781
emi-hi <emily.hillier@gmail.com>
2021-08-12 12:56:52 -0700

    -adds supplier information component

--- frontend/src/supplementary/SupplementaryContainer.js
+++ frontend/src/supplementary/SupplementaryContainer.js
@@ -9,7 +9,7 @@ const SupplementaryContainer = (props) => {
   const [details, setDetails] = useState({});
   const [loading, setLoading] = useState(true);
   const [comment, setComment] = useState('');
-  const [newData, setNewData] = useState({})
+  const [newData, setNewData] = useState({});
   const { keycloak, user } = props;
 
   const handleAddComment = (comment) => {
@@ -17,15 +17,50 @@ const SupplementaryContainer = (props) => {
   };
 
   const handleSubmit = (status) => {
-    setNewData({...newData, status })
+    setNewData({ ...newData, status });
     axios.post(ROUTES_SUPPLEMENTARY.DETAILS.replace(':id', id), details);
   };
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setNewData({ ...newData, [name]: value });
+    console.log(newData);
   };
   const refreshDetails = () => {
+    const data = {
+      legalName: 'test',
+      organizationAddress: [{
+        addressLine1: '1004 CATHERINE ST',
+        addressLine2: null,
+        addressLine3: null,
+        addressType: { addressType: 'Records' },
+        city: 'VICTORIA',
+        country: 'Canada',
+        county: null,
+        id: 209,
+        postalCode: 'V9A 3V4',
+        representativeName: 'Emily Hillier',
+        state: 'BC',
+      }, {
+        addressLine1: 'TEST ST',
+        addressLine2: null,
+        addressLine3: null,
+        addressType: { addressType: 'Service' },
+        city: 'VICTORIA',
+        country: 'Canada',
+        county: null,
+        id: 209,
+        postalCode: 'V9A 3V4',
+        representativeName: 'Emily Hillier',
+        state: 'BC',
+      }],
+      makes: ['KIA', 'JEEP', 'BMW'],
+    };
+    const serviceAddress = data.organizationAddress.find((each) => each.addressType.addressType === 'Service');
+    const recordsAddress = data.organizationAddress.find((each) => each.addressType.addressType === 'Records');
+    setDetails({
+      legalName: data.legalName, serviceAddress, recordsAddress, makes: data.makes,
+    });
     // setLoading(true);
     // axios.get(ROUTES_SUPPLEMENTARY.DETAILS.replace(':id', id)).then((response) => {
     //   setDetails(response.details);
--- frontend/src/supplementary/components/SupplierInformation.js
+++ frontend/src/supplementary/components/SupplierInformation.js
@@ -5,21 +5,74 @@ const SupplierInformation = (props) => {
   const { details, handleInputChange, newData } = props;
   return (
     <>
-      <div className="text-blue">
+      <div className="text-blue mb-4">
         Make the required updates in the fields next to the original submitted values and provide an explanation in the comment box at the bottom of this form.
       </div>
-      <div>
-        <TextInput
-          label="Legal Name"
-          id="legalName"
-          name="legalName"
-          defaultValue={newData.legalName}
-          handleInputChange={(event) => {
-            handleInputChange(event);
-          }}
-          labelSize="d-inline-block col-sm-3"
-          inputSize="d-inline-block align-middle col-sm-5"
-          rowSize=""
+      <div className="my-4">
+        <label
+          className="d-inline-block col-sm-4 text-blue font-weight-bold"
+          htmlFor="serviceAddress"
+        >
+          Legal Name
+        </label>
+        <div className="d-inline-block align-middle col-sm-5 p-0">
+          <input
+            className="form-control"
+            id="legalName"
+            name="legalName"
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="my-4">
+        <label
+          className="d-inline-block col-sm-4 text-blue font-weight-bold"
+          htmlFor="serviceAddress"
+        >
+          Service Address
+        </label>
+        <textarea
+          className="form-control d-inline-block align-middle col-sm-5"
+          id="serviceAddress"
+          name="serviceAddress"
+            // type={type}
+          value={newData.serviceAddress}
+          onChange={handleInputChange}
+          min="0"
+        />
+      </div>
+      <div className="my-4">
+        <label
+          className="d-inline-block col-sm-4 text-blue font-weight-bold"
+          htmlFor="recordsAddress"
+        >
+          Records Address
+        </label>
+        <textarea
+          className="form-control d-inline-block align-middle col-sm-5"
+          id="recordsAddress"
+          name="recordsAddress"
+            // type={type}
+          value={newData.recordsAddress}
+          onChange={handleInputChange}
+          min="0"
+        />
+      </div>
+      <div className="my-4">
+        <label
+          className="d-inline-block col-sm-4 text-blue font-weight-bold"
+          htmlFor="lightDutyMakes"
+        >
+          Light Duty Vehicle Makes
+        </label>
+        <textarea
+          className="form-control d-inline-block align-middle col-sm-5"
+          id="lightDutyMakes"
+          name="lightDutyMakes"
+            // type={type}
+          value={newData.lightDutyMakes}
+          onChange={handleInputChange}
+          min="0"
         />
       </div>
     </>
