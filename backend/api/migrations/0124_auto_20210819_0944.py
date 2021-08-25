a6928fe497487d81c886d3244552f9b71a611f98
emi-hi <emily.hillier@gmail.com>
2021-08-25 08:08:35 -0700

    fixed migration conflicts

--- backend/api/migrations/0124_auto_20210819_0944.py
+++ backend/api/migrations/0124_auto_20210819_0944.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0122_auto_20210816_1433'),
+        ('api', '0123_supplementalreportattachment'),
     ]
 
     operations = [
