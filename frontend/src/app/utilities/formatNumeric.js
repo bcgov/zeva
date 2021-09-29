741a69af2e95c6df24b1935ea267aee1bd306d4f
emi-hi <emily.hillier@gmail.com>
2021-09-29 09:50:09 -0700

    -adds extra argument and logic to formatnumeric to show brackets instead of minus symbol for negative balances -adds extra argument in formatNumeric call to spots where balance is listed

--- frontend/src/app/utilities/formatNumeric.js
+++ frontend/src/app/utilities/formatNumeric.js
@@ -1,4 +1,4 @@
-const formatNumeric = (value, decimals = 2) => {
+const formatNumeric = (value, decimals = 2, roundBracket) => {
   let newValue = value;
   if (isNaN(newValue)) {
     return newValue;
@@ -11,12 +11,17 @@ const formatNumeric = (value, decimals = 2) => {
   } else if (decimals === 0) {
     newValue = Math.round(newValue);
   }
-
+  if (roundBracket) {
+    if (newValue < 0) {
+      newValue = `(${Math.abs(newValue)})`;
+    }
+  }
   if (typeof newValue === 'number') {
     newValue = newValue.toString();
   }
 
   newValue = newValue.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+
 
   return newValue;
 };
