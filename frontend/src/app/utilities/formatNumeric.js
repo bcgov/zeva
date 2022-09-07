56164e80eaf64bebe16a6b17c1147dd03244d39a
AlexZorkin <alex@bigthink.io>
2022-09-07 00:16:08 -0700

    chore: linting

--- frontend/src/app/utilities/formatNumeric.js
+++ frontend/src/app/utilities/formatNumeric.js
@@ -11,8 +11,8 @@ const formatNumeric = (value, decimals = 2, roundBracket) => {
   } else if (decimals === 0) {
     newValue = Math.round(newValue);
   }
-  if(Math.abs(value) < 0.001) {
-    newValue = "0.00";
+  if (Math.abs(value) < 0.001) {
+    newValue = '0.00';
   }
   if (roundBracket) {
     if (newValue < 0) {
--- frontend/src/credits/components/__tests__/CreditTransactionListTable.test.js
+++ frontend/src/credits/components/__tests__/CreditTransactionListTable.test.js
@@ -6,46 +6,56 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 const data = [
   {
-    creditClass: {creditClass: "A"},
+    creditClass: { creditClass: 'A' },
     detailTransactionType: null,
     foreignKey: 159,
-    modelYear: {name: "2019", effectiveDate: "2019-01-01", expirationDate: "2019-12-31"},
+    modelYear: {
+      name: '2019',
+      effectiveDate: '2019-01-01',
+      expirationDate: '2019-12-31'
+    },
     totalValue: 10.14,
-    transactionTimestamp: "2021-08-31T10:54:43.299000-07:00",
-    transactionType: {transactionType: "Validation"},
+    transactionTimestamp: '2021-08-31T10:54:43.299000-07:00',
+    transactionType: { transactionType: 'Validation' }
   },
   {
-    creditClass: {creditClass: "A"},
+    creditClass: { creditClass: 'A' },
     detailTransactionType: null,
     foreignKey: 218,
-    modelYear: {name: "2020", effectiveDate: "2020-01-01", expirationDate: "2020-12-31"},
+    modelYear: {
+      name: '2020',
+      effectiveDate: '2020-01-01',
+      expirationDate: '2020-12-31'
+    },
     totalValue: 48,
-    transactionTimestamp: "2022-02-25T10:21:11.207083-08:00",
-    transactionType: {transactionType: "Validation"}
+    transactionTimestamp: '2022-02-25T10:21:11.207083-08:00',
+    transactionType: { transactionType: 'Validation' }
   },
-  {
-    
-  }
+  {}
 ];
 
 const reports = [
   {
-    compliant: "Yes",
+    compliant: 'Yes',
     id: 191,
     ldvSales: 5000,
-    modelYear: {name: "2020", effectiveDate: "2020-01-01", expirationDate: "2020-12-31"},
-    effectiveDate: "2020-01-01",
-    expirationDate: "2020-12-31",
-    name: "2020",
+    modelYear: {
+      name: '2020',
+      effectiveDate: '2020-01-01',
+      expirationDate: '2020-12-31'
+    },
+    effectiveDate: '2020-01-01',
+    expirationDate: '2020-12-31',
+    name: '2020',
     obligationCredits: 0,
     obligationTotal: 0,
-    organizationName: "Tesla Motors Canada ULC",
+    organizationName: 'Tesla Motors Canada ULC',
     supplementalId: null,
-    supplementalStatus: "ASSESSED",
-    supplierClass: "L",
-    validationStatus: "ASSESSED",
-  } 
-]
+    supplementalStatus: 'ASSESSED',
+    supplierClass: 'L',
+    validationStatus: 'ASSESSED'
+  }
+];
 
 it('renders without crashing', () => {
   render(
