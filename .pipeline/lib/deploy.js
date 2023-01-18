bb3e05fdf2c9b259a0e23ee2b788cd81005df38c
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2023-01-17 16:19:40 -0800

    updates for dev env based pipeline (#1293)

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -10,91 +10,31 @@ module.exports = settings => {
   const changeId = phases[phase].changeId;
   const oc = new OpenShiftClientX(Object.assign({namespace: phases[phase].namespace}, options));
 
-  //add Valid Redirect URIs for the pull request to keycloak
-  if(phase === 'dev') {
-    const kc = new KeyCloakClient(settings, oc);
-    kc.addUris();
-  }
-
   const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, "../../openshift"));
   var objects = [];
 
   // The deployment of your cool app goes here ▼▼▼
 
-  //create network security policies for internal pod to pod communications
-  if(phase === 'dev') {
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/knp/knp-env-pr.yaml`, {
-      'param': {
-        'SUFFIX': phases[phase].suffix,
-        'ENVIRONMENT': phases[phase].phase
-      }
-    }))
-  }
-
   // create configs
-  if(phase === 'dev') {
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/config/configmap.yaml`, {
-      'param': {
-        'NAME': phases[phase].name,
-        'SUFFIX': phases[phase].suffix,
-        'ENV_NAME': phases[phase].phase,
-        'HOST_NAME': phases[phase].host,
-        'BACKEND_HOST_NAME': phases[phase].backendHost,
-        'SSO_NAME': phases[phase].ssoName,
-        'KEYCLOAK_REALM': 'standard',
-        'KEYCLOAK_CALLBACK_URL': phases[phase].keycloakCallbackUrl,
-        'KEYCLOAK_SITEMINDER_LOGOUT_REDIRECT_URL': phases[phase].keycloakSiteminderLogoutRedirectUrl,
-        'DJANGO_DEBUG': phases[phase].djangoDebug,
-        'OCP_NAME': phases[phase].ocpName,
-        'LOGOUT_HOST_NAME': phases[phase].logoutHostName,
-      }
-    }))
-  }
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/config/configmap.yaml`, {
+    'param': {
+      'NAME': phases[phase].name,
+      'SUFFIX': phases[phase].suffix,
+      'ENV_NAME': phases[phase].phase,
+      'HOST_NAME': phases[phase].host,
+      'BACKEND_HOST_NAME': phases[phase].backendHost,
+      'SSO_NAME': phases[phase].ssoName,
+      'KEYCLOAK_REALM': 'standard',
+      'KEYCLOAK_CALLBACK_URL': phases[phase].keycloakCallbackUrl,
+      'KEYCLOAK_SITEMINDER_LOGOUT_REDIRECT_URL': phases[phase].keycloakSiteminderLogoutRedirectUrl,
+      'DJANGO_DEBUG': phases[phase].djangoDebug,
+      'OCP_NAME': phases[phase].ocpName,
+      'LOGOUT_HOST_NAME': phases[phase].logoutHostName,
+    }
+  }))
 
-  if(phase === 'dev') {
-    //deploy Patroni required secrets
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/patroni/deployment-prereq.yaml`, {
-      'param': {
-        'NAME': 'patroni',
-        'SUFFIX': phases[phase].suffix
-      }
-    }))
-    //deploy Patroni
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/patroni/deployment.yaml`, {
-      'param': {
-        'NAME': 'patroni',
-        'ENV_NAME': phases[phase].phase,
-        'SUFFIX': phases[phase].suffix,
-        'CPU_REQUEST': phases[phase].patroniCpuRequest,
-        'CPU_LIMIT': phases[phase].patroniCpuLimit,
-        'MEMORY_REQUEST': phases[phase].patroniMemoryRequest,
-        'MEMORY_LIMIT': phases[phase].patroniMemoryLimit,
-        'IMAGE_REGISTRY': 'image-registry.openshift-image-registry.svc:5000',
-        'IMAGE_STREAM_NAMESPACE': phases[phase].namespace,
-        'IMAGE_STREAM_TAG': 'patroni:v10-stable',
-        'REPLICA': phases[phase].patroniReplica,
-        'PVC_SIZE': phases[phase].patroniPvcSize,
-        'STORAGE_CLASS': phases[phase].storageClass
-      }
-    }))
-  }
-  
-  //only deploy rabbitmq secret and configmap
-  if(phase === 'dev') {
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/rabbitmq/rabbitmq-secret-configmap-only.yaml`, {
-      'param': {
-        'NAME': phases[phase].name,
-        'ENV_NAME': phases[phase].phase,
-        'SUFFIX': phases[phase].suffix,
-        'NAMESPACE': phases[phase].namespace,
-        'CLUSTER_NAME': 'rabbitmq-cluster'
-      }
-    }))
-  }
-
+ 
   // deploy frontend configmap
-  //if(phase === 'dev') {
-  //add CREDIT_AGREEMENTS_ENABLED for release-1.29.0
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-configmap.yaml`, {
     'param': {
       'NAME': phases[phase].name,
@@ -113,7 +53,6 @@ module.exports = settings => {
       'SUPPLEMENTAL_ENABLED': phases[phase].supplementalEnabled
     }
   }))
-  //}
 
   // deploy frontend
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-dc.yaml`, {
@@ -149,85 +88,6 @@ module.exports = settings => {
     }
   })) 
   
-  //deploy schemaspy
-  /*
-  if(phase === 'dev') {
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/schemaspy/schemaspy-dc.yaml`, {
-      'param': {
-        'SUFFIX': phases[phase].suffix,
-        'ENV_NAME': phases[phase].phase,
-        'CPU_REQUEST': phases[phase].schemaspyCpuRequest,
-        'CPU_LIMIT': phases[phase].schemaspyCpuLimit,
-        'MEMORY_REQUEST': phases[phase].schemaspyMemoryRequest,
-        'MEMORY_LIMIT': phases[phase].schemaspyMemoryLimit,
-        'HEALTH_CHECK_DELAY': phases[phase].schemaspyHealthCheckDelay,
-        'OCP_NAME': phases[phase].ocpName
-      }
-    }))
-  }*/
-
-  /** 
-  //deploy rabbitmq, use docker image directly
-  //POST_START_SLEEP is harded coded in the rabbitmq template, replacement was not successful
-  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/rabbitmq/rabbitmq-cluster-dc.yaml`, {
-    'param': {
-      'NAME': phases[phase].name,
-      'ENV_NAME': phases[phase].phase,
-      'SUFFIX': phases[phase].suffix,
-      'NAMESPACE': phases[phase].namespace,
-      'CLUSTER_NAME': 'rabbitmq-cluster',
-      'ISTAG': `image-registry.openshift-image-registry.svc:5000/${phases[phase].namespace}/rabbitmq:3.8.3-management`,
-      'SERVICE_ACCOUNT': 'rabbitmq-discovery',
-      'VOLUME_SIZE': phases[phase].rabbitmqPvcSize,
-      'CPU_REQUEST': phases[phase].rabbitmqCpuRequest,
-      'CPU_LIMIT': phases[phase].rabbitmqCpuLimit,
-      'MEMORY_REQUEST': phases[phase].rabbitmqMemoryRequest,
-      'MEMORY_LIMIT': phases[phase].rabbitmqMemoryLimit,
-      'REPLICA': phases[phase].rabbitmqReplica,
-      'POST_START_SLEEP': phases[phase].rabbitmqPostStartSleep,
-      'STORAGE_CLASS': phases[phase].storageClass
-    }
-  }))
-  */
-
-  /*** remove minio deployment in pr pipeline, one pre-deployed minio will serve all prs
-   * minio configurations stay in config.js unchanged
-  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/minio/minio-dc.yaml`, {
-    'param': {
-      'NAME': phases[phase].name,
-      'SUFFIX': phases[phase].suffix,
-      'ENV_NAME': phases[phase].phase,
-      'PVC_SIZE': phases[phase].minioPvcSize,
-      'CPU_REQUEST': phases[phase].minioCpuRequest,
-      'CPU_LIMIT': phases[phase].minioCpuLimit,
-      'MEMORY_REQUEST': phases[phase].minioMemoryRequest,
-      'MEMORY_LIMIT': phases[phase].minioMemoryRequest      
-    }
-  }))
-  */
-
-  //add autoacaler
-  /*****
-  if(phase === 'test' || phase === 'prod') {
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-autoscaler.yaml`, {
-      'param': {
-        'NAME': phases[phase].name,
-        'SUFFIX': phases[phase].suffix,
-        'MIN_REPLICAS': phases[phase].frontendMinReplicas,
-        'MAX_REPLICAS': phases[phase].frontendMaxReplicas
-      }
-    }))
-    objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/backend/backend-autoscaler.yaml`, {
-      'param': {
-        'NAME': phases[phase].name,
-        'SUFFIX': phases[phase].suffix,
-        'MIN_REPLICAS': phases[phase].backendMinReplicas,
-        'MAX_REPLICAS': phases[phase].backendMaxReplicas
-      }
-    }))
-  }
-  ********/
-
   oc.applyRecommendedLabels(
       objects,
       phases[phase].name,
