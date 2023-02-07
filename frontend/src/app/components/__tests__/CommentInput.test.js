3a37e9bf88c0ae17eb9d51afd2e6d8bbca89e571
julianforeman <julianforeman@me.com>
2023-02-06 16:09:18 -0800

    Added front-end tests for Alert, Button, Comment and CommentInput components.  Also updated the CommentInput component to add id's to make testing it easier.

---
+++ frontend/src/app/components/__tests__/CommentInput.test.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import CommentInput from '../CommentInput';
+
+jest.mock('react-quill', () => {
+  return jest.fn(props => {
+    return (
+      <textarea
+        data-testid={props['data-testid']}
+        readOnly={props.readOnly}
+        defaultValue={props.defaultValue}
+        onChange={props.onChange}
+      />
+    );
+  });
+});
+
+describe('CommentInput component', () => {
+  it('renders correctly', () => {
+    const title = 'Comment Title';
+    const buttonText = 'Add Comment';
+    const handleCommentChange = jest.fn();
+    const handleAddComment = jest.fn();
+    const { getByTestId, getByText } = render(
+      <CommentInput
+        title={title}
+        handleCommentChange={handleCommentChange}
+        handleAddComment={handleAddComment}
+        buttonText={buttonText}
+        testid="comment-input"
+        buttontestid="add-comment-button"
+      />
+    );
+
+    expect(getByTestId('comment-input')).toBeTruthy();
+    expect(getByText(buttonText)).toBeTruthy();
+  });
+
+  it('calls handleCommentChange when the textarea changes', () => {
+    const title = 'Comment Title';
+    const buttonText = 'Add Comment';
+    const handleCommentChange = jest.fn();
+    const handleAddComment = jest.fn();
+    const { getByTestId } = render(
+      <CommentInput
+        title={title}
+        handleCommentChange={handleCommentChange}
+        handleAddComment={handleAddComment}
+        buttonText={buttonText}
+        testid="comment-input"
+        buttontestid="add-comment-button"
+      />
+    );
+
+    const commentInput = getByTestId('comment-input');
+    fireEvent.change(commentInput, { target: { value: 'Hello World' } });
+    expect(handleCommentChange).toHaveBeenCalled();
+  });
+
+  it('calls handleAddComment when the button is clicked', () => {
+    const title = 'Comment Title';
+    const buttonText = 'Add Comment';
+    const handleCommentChange = jest.fn();
+    const handleAddComment = jest.fn();
+    const { getByTestId, getByText } = render(
+      <CommentInput
+        title={title}
+        handleCommentChange={handleCommentChange}
+        handleAddComment={handleAddComment}
+        buttonText={buttonText}
+        data-testid="comment-input"
+        buttontestid="add-comment-button"
+      />
+    );
+
+    const addCommentButton = getByText(buttonText);
+    fireEvent.click(addCommentButton);
+    expect(handleAddComment).toHaveBeenCalled();
+  });
+});
