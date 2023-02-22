6c465c5512b3df7b1960442bec9838d87043d437
julianforeman <julianforeman@me.com>
2023-02-22 14:22:46 -0800

    Tests for the Disclaimer and DisplayComment components

---
+++ frontend/src/app/components/__tests__/Disclaimer.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Disclaimer from '../Disclaimer';
+
+describe('Disclaimer component', () => {
+  it('should render without crashing', () => {
+    render(<Disclaimer />);
+  });
+
+  it('should render the disclaimer text', () => {
+    const { getByTestId } = render(<Disclaimer />);
+    const contentElement = getByTestId('content');
+    expect(contentElement.textContent).toEqual(
+      'This information does not replace or constitute legal advice. Users are responsible for ensuring compliance with the Zero-Emissions Vehicles Act and Regulations.'
+    );
+  });
+});
