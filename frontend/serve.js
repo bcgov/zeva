7e4cc277c257a10776769512b9a08cd72c9591a9
AlexZorkin <alex@bigthink.io>
2023-03-02 13:07:25 -0800

    fix: updated webpack config to use index.html

--- frontend/serve.js
+++ frontend/serve.js
@@ -1,22 +1,22 @@
-const http = require('http');
-const express = require('express');
-const fallback = require('express-history-api-fallback');
-const notifications = require('./notifications');
-const morgan = require('morgan');
+const http = require('http')
+const express = require('express')
+const fallback = require('express-history-api-fallback')
+const notifications = require('./notifications')
+const morgan = require('morgan')
 
 const websocketServer = http.createServer((req, res) => {
-  res.end();
-});
+  res.end()
+})
 
-const io = require('socket.io')(websocketServer);
+const io = require('socket.io')(websocketServer)
 
-notifications.setup(io);
-websocketServer.listen(5002, '0.0.0.0');
+notifications.setup(io)
+websocketServer.listen(5002, '0.0.0.0')
 
-const app = express();
-app.use(morgan('combined'));
+const app = express()
+app.use(morgan('combined'))
 
-const root = `${__dirname}/public/build`;
-app.use(express.static(root));
-app.use(fallback('generated_index.html', { root }));
-app.listen(3000);
+const root = `${__dirname}/public/build`
+app.use(express.static(root))
+app.use(fallback('index.html', { root }))
+app.listen(3000)
