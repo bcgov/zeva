910067f1c21406446f79ce9360d41de7a59dda65
Alex Zorkin <47334977+AlexZorkin@users.noreply.github.com>
2023-03-20 10:18:20 -0700

    fix: display comment unit test fix (#1529)

--- frontend/src/app/components/__tests__/DisplayComment.test.js
+++ frontend/src/app/components/__tests__/DisplayComment.test.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import DisplayComment from '../DisplayComment';
+import React from 'react'
+import { render } from '@testing-library/react'
+import DisplayComment from '../DisplayComment'
 
 describe('DisplayComment component', () => {
   it('should render comments properly', () => {
@@ -8,32 +8,32 @@ describe('DisplayComment component', () => {
       {
         id: 1,
         createUser: {
-          displayName: 'John',
+          displayName: 'John'
         },
-        createTimestamp: '2022-02-21T10:00:00Z',
-        comment: '<p>Hello world!</p>',
+        createTimestamp: '2022-02-21T10:00',
+        comment: '<p>Hello world!</p>'
       },
       {
         id: 2,
         createUser: {
-          displayName: 'Mary',
+          displayName: 'Mary'
         },
-        createTimestamp: '2022-02-22T11:00:00Z',
-        comment: '<p>Hi there!</p>',
-      },
-    ];
-    const { getAllByText } = render(<DisplayComment commentArray={commentArray} />);
-    expect(getAllByText(/Comments/)).toBeTruthy();
-    expect(getAllByText(/John/)).toBeTruthy();
-    expect(getAllByText(/Mary/)).toBeTruthy();
-    expect(getAllByText(/2022-02-21 10:00 am/)).toBeTruthy();
-    expect(getAllByText(/2022-02-22 11:00 am/)).toBeTruthy();
-    expect(getAllByText(/Hello world!/)).toBeTruthy();
-    expect(getAllByText(/Hi there!/)).toBeTruthy();
-  });
+        createTimestamp: '2022-02-22T11:00',
+        comment: '<p>Hi there!</p>'
+      }
+    ]
+    const { getAllByText } = render(<DisplayComment commentArray={commentArray} />)
+    expect(getAllByText(/Comments/)).toBeTruthy()
+    expect(getAllByText(/John/)).toBeTruthy()
+    expect(getAllByText(/Mary/)).toBeTruthy()
+    expect(getAllByText(/2022-02-21 10:00 am/)).toBeTruthy()
+    expect(getAllByText(/2022-02-22 11:00 am/)).toBeTruthy()
+    expect(getAllByText(/Hello world!/)).toBeTruthy()
+    expect(getAllByText(/Hi there!/)).toBeTruthy()
+  })
 
   it('should render nothing when commentArray is empty', () => {
-    const { container } = render(<DisplayComment />);
-    expect(container.querySelector('span').firstChild).toBeNull();
-  });
-});
+    const { container } = render(<DisplayComment />)
+    expect(container.querySelector('span').firstChild).toBeNull()
+  })
+})
