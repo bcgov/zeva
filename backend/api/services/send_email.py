58dae40b29f92d8de701408bbe6e3bf96f49108d
tim738745 <tim.chen@gov.bc.ca>
2023-06-19 11:16:34 -0700

    fix: zeva-1783 - various fixes

--- backend/api/services/send_email.py
+++ backend/api/services/send_email.py
@@ -579,7 +579,7 @@ def send_credit_agreement_emails(notifications, agreement):
         print('send_credit_agreement_emails')
     
     request_type = 'credit_agreement'
-    user_emails = get_subscribed_user_emails(notifications, agreement.update_user, request_type)
+    user_emails = get_subscribed_user_emails(notifications, agreement, request_type)
     notification_objects = get_notification_objects(notifications)
     test_info = prepare_test_info(agreement, notification_objects)
 
