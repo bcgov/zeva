46a6158b1e9fc93b537e766201c5a54c55fdae02
julianforeman <julianforeman@me.com>
2023-07-05 13:57:14 -0700

    Accidentally overwrote wrong file, restoring to normal

--- openshift/scripts/import-data.sh
+++ openshift/scripts/import-data.sh
@@ -1,76 +1,66 @@
 #!/bin/bash
 set -e
 
-# This script takes a .sql file from your local machine
-# automatically uploads it into 
-# an openshift pod in the test environment
-# downloads a backup to your local machine
-# resets the existing database
-# then executes the uploaded sql file.
+# This script creates a pg_dump .tar file from an openshift pod
+# and automatically downloads and restores the .tar into 
+# a local postgres docker container
 
-# Unlike copying from openshift to your local machine, you cannot copy single files when copying from your local machine to openshift
-# But you can copy directories
-# 3 Arguments
-# $1 = path to directory (see argument 2)
-# $2 = name of directory containing the .sql file you want to execute
-# $3 = the name of the .sql file
-# example command:
-# bash run-test-script.sh /Users/johndoe/Desktop zeva-scripts zeva-reset.sql
+# 2 Arguments 
+# $1 = 'test' or 'prod'
+# #2 = 'local container name or id'
+# example command
+# . import-data.sh test 398cd4661173
 
-if [ "$#" -ne 3 ]; then
-    echo "Passed $# parameters. Expected 3."
+if [ "$#" -ne 2 ]; then
+    echo "Passed $# parameters. Expected 2."
     exit 1
 fi
 
-project_name='e52f12-dev'
-# the lead test db pod:
-pod_name='zeva-spilo-dev-0'
-env='dev'
+# defaults set to test
+project_name='e52f12-test'
+pod_name='zeva-spilo-test-0'
+env='test'
 
-local_path=$1
-directory_name=$2
-sql_file_name=$3
+local_container=$2
 
+# checks if you are logged in to openshift
 echo "** Checking Openshift creds"
 oc whoami
 echo "logged in"
 echo
 
+# prod variables
+if [ $1 = "prod" ]; then
+  project_name='e52f12-prod'
+  pod_name='zeva-spilo-prod-0'
+  env='prod'
+else
+  echo '** Using test variables'
+  echo
+fi
+
 echo "** Setting project $env"
 oc project $project_name
 echo
 
-echo "** Uploading the .sql file"
-oc rsync $local_path/$directory_name $pod_name:/tmp
+echo "** Starting pg_dump"
+oc exec $pod_name -- bash -c 'pg_dump -U postgres -F t --no-privileges --no-owner -c -d zeva > /tmp/zeva.tar'
 echo
 
-echo "** Creating DB backup"
-oc exec $pod_name -- bash -c "pg_dump zeva -U postgres > /tmp/$directory_name/script_db_backup.sql"
+echo "** Downloading .tar file"
+oc rsync $pod_name:/tmp/zeva.tar ./
 echo
 
-echo "** Downloading DB backup"
-oc rsync $pod_name:/tmp/$directory_name/db_backup.sql $local_path/$directory_name
+echo "** Copying .tar to local database container $local_container"
+docker cp zeva.tar $local_container:/tmp/zeva.tar
 echo
 
-echo "** Emptying DB for new .sql file"
-oc exec $pod_name -- bash -c "psql -d zeva -c 'DROP SCHEMA public CASCADE;'"
-echo
-
-echo "** Creating empty DB schema"
-oc exec $pod_name -- bash -c "psql -d zeva -c 'CREATE SCHEMA public;'"
-echo
-
-echo "** Granting permissions on new schema"
-oc exec $pod_name -- bash -c "psql -d zeva -c 'GRANT ALL ON SCHEMA public TO postgres;'"
-oc exec $pod_name -- bash -c "psql -d zeva -c 'GRANT ALL ON SCHEMA public TO public;'"
-oc exec $pod_name -- bash -c "psql -d zeva -c 'GRANT ALL ON SCHEMA public TO zevayhn;'"
-echo
-
-echo "** Running the .sql file"
-oc exec $pod_name -- bash -c 'psql -d zeva -U zevayhn -f /tmp/'"$directory_name"'/'"$sql_file_name"''
+echo "** Restoring local database"
+docker exec $local_container bash -c 'pg_restore -U postgres -F t --no-privileges --no-owner -c -d zeva < /tmp/zeva.tar' || true
+docker exec $local_container bash -c 'pg_restore -U postgres -F t --no-privileges --no-owner -c -d zeva < /tmp/zeva.tar'
 echo
 
 echo "** Cleaning up"
-oc exec $pod_name -- rm -r /tmp/$directory_name
+rm zeva.tar
 
-echo "** Finished running the script"
+echo "** Finished database restore"
