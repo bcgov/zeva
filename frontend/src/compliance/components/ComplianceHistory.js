46f70ac15a5cb32988dd430ef40eefbfb58b5474
tim738745 <tim.chen@gov.bc.ca>
2023-08-09 16:50:56 -0700

    fix: zeva-1900 - reassessment history bug fix

--- frontend/src/compliance/components/ComplianceHistory.js
+++ frontend/src/compliance/components/ComplianceHistory.js
@@ -41,48 +41,69 @@ const ComplianceHistory = (props) => {
       })
   }, [])
 
+  // assumes passed in history is in order from most recent to earliest
+  // returns a history that is also from most recent to earliest
+  const removeSequentialRecommendations = (history) => {
+    const result = []
+    let previousItemStatusIsRecommended = false
+    const reversedHistory = history.toReversed()
+    reversedHistory.forEach((item) => {
+      const itemStatus = item.status
+      if (itemStatus === 'RECOMMENDED') {
+        if (!previousItemStatusIsRecommended) {
+          result.unshift(item)
+        }
+        previousItemStatusIsRecommended = true
+      } else {
+        result.unshift(item)
+        previousItemStatusIsRecommended = false
+      }
+    })
+    return result
+  }
+
   const getHistory = (itemHistory, reportType) => {
     const tempHistory = []
     if (itemHistory) {
       itemHistory.forEach((obj, i) => {
-        if(i === 0 && reportType.includes("Model Year Report")){
+        if (i === 0 && reportType.includes('Model Year Report')) {
           tempHistory.push(itemHistory[itemHistory.length - 1])
-        }
-        else if (['DRAFT'].indexOf(obj.status) >= 0) {
+        } else if (['DRAFT'].indexOf(obj.status) >= 0) {
           const found = tempHistory.findIndex(
             (each) => ['DRAFT'].indexOf(each.status) >= 0
           )
           // Check to see if a report has been returned to a draft status from submitted and if it has
           // Then we need to modify the recorded object to correctly display the returned status instead of draft
           // We also never capture returns in a reassessment because it is strictly internal
-          if (itemHistory[i + 1]?.status === "SUBMITTED" && !reportType.includes("Reassessment")){
-            let actuallyReturned = {...obj}
-            actuallyReturned.status = "RETURNED"
+          if (itemHistory[i + 1]?.status === 'SUBMITTED' && !reportType.includes('Reassessment')) {
+            const actuallyReturned = { ...obj }
+            actuallyReturned.status = 'RETURNED'
             tempHistory.push(actuallyReturned)
-          }
-          else if (found < 0 && ['DRAFT', 'SUBMITTED'].includes(obj.status)){
+          } else if (found < 0 && ['DRAFT', 'SUBMITTED'].includes(obj.status)) {
             tempHistory.push(obj)
           }
-        }
-        else if (['SUBMITTED'].indexOf(obj.status) >= 0) {
+        } else if (['SUBMITTED'].indexOf(obj.status) >= 0) {
           // Check if a submitted history status comes after a returned/draft status
           // We capture this as it has been re-submitted
-          if(["RETURNED", "DRAFT"].includes(itemHistory[i + 1]?.status)){
+          if (['RETURNED', 'DRAFT'].includes(itemHistory[i + 1]?.status)) {
             tempHistory.push(obj)
           }
-        }
-        else if (
+        } else if (
           ['RETURNED'].indexOf(obj.status) >= 0
         ) {
           // Checking to find any return to a supplier, we don't want to capture when a director returns to an analyst
           // The history reads from most recent to least recent so i + 1 to check the previous status
           // There is a case where the entry before a return is Recommended but it is correctly returned to a supplier so we also need to look at i - 1 to see if it is submitted
-          if(["RETURNED", "SUBMITTED"].includes(itemHistory[i + 1]?.status) && !reportType.includes("Reassessment") || itemHistory[i - 1]?.status === "SUBMITTED" && !reportType.includes("Reassessment")){
-              tempHistory.push(obj)
+          if ((['RETURNED', 'SUBMITTED'].includes(itemHistory[i + 1]?.status) && !reportType.includes('Reassessment')) || (itemHistory[i - 1]?.status === 'SUBMITTED' && !reportType.includes('Reassessment'))) {
+            tempHistory.push(obj)
           }
+        } else if (
+          ['RECOMMENDED'].indexOf(obj.status) >= 0
+        ) {
+          tempHistory.push(obj)
         }
         if (
-          ['RECOMMENDED', 'ASSESSED', 'REASSESSED'].indexOf(obj.status) >= 0
+          ['ASSESSED', 'REASSESSED'].indexOf(obj.status) >= 0
         ) {
           const found = tempHistory.findIndex(
             (each) => obj.status === each.status
@@ -95,10 +116,10 @@ const ComplianceHistory = (props) => {
     }
     // If we are in a model year report it will have a created status at the top
     // Therefore we need to move the first entry to the end of the history array
-    if(reportType.includes("Model Year Report")){
+    if (reportType.includes('Model Year Report')) {
       tempHistory.push(tempHistory.shift())
     }
-    return tempHistory
+    return removeSequentialRecommendations(tempHistory)
   }
   const getTitle = (item) => {
     const type = item.isSupplementary ? startedAsSupplemental ? 'Supplementary Report' : 'Reassessment' : 'Model Year Report'
@@ -247,7 +268,7 @@ const ComplianceHistory = (props) => {
                         }
                       }}
                     >
-                     {getTitle(item)}
+                      {getTitle(item)}
                     </button>
                   </h2>
                 </div>
