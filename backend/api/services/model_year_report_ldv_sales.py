870df64dce1ccce91294f47c7432b28bfd6feed4
tim738745 <98717409+tim738745@users.noreply.github.com>
2023-08-22 13:16:36 -0700

    fix: zeva-1926 - reassessment-ldv-sales (#1927)

---
+++ backend/api/services/model_year_report_ldv_sales.py
@@ -0,0 +1,15 @@
+from api.models.model_year_report_ldv_sales import ModelYearReportLDVSales
+
+
+def get_most_recent_ldv_sales(model_year_report):
+    ldv_sales_obj = (
+        ModelYearReportLDVSales.objects.filter(
+            model_year=model_year_report.model_year, model_year_report=model_year_report
+        )
+        .order_by("-create_timestamp")
+        .only("ldv_sales")
+        .first()
+    )
+    if ldv_sales_obj:
+        return ldv_sales_obj.ldv_sales
+    return None
