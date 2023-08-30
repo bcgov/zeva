561a5a5ea9a49dad829c992e2bac781e0c9cfb7f
julianforeman <julianforeman@me.com>
2023-08-30 11:39:17 -0700

    Commenting out old tests that will be moved to new component file to build environment for testing

--- frontend/src/credits/components/__tests__/AnalystRecommendationHeader.test.js
+++ frontend/src/credits/components/__tests__/AnalystRecommendationHeader.test.js
@@ -38,57 +38,57 @@ describe('AnalystRecommendationHeader', () => {
     )
   })
 
-  describe('renders certain elements based on validation status and user permissions', () => {
-    const runTests = (statuses, permissions, element, numberOfOccurences) => {
-      for (const status of statuses) {
-        for (const permission of permissions) {
-          test(`renders "${element}" element when validation status is ${status} and user has permission ${permission}`, () => {
-            const submission = {
-              validationStatus: status
-            }
-            const user = {
-              hasPermission: getHasPermission(permission)
-            }
-            const props = { ...baseProps, submission, user }
-            const { queryAllByText } = render(
-              <Router>
-                <AnalystRecommendationHeader
-                  {...props}
-                />
-              </Router>
-            )
-            expect(queryAllByText('Analyst Recommendation')).toHaveLength(numberOfOccurences)
-          })
-        }
-      }
-    }
-    runTests(['RECOMMEND_APPROVAL', 'RECOMMEND_REJECTION', 'CHECKED', 'REJECTED', 'VALIDATED'], ['RECOMMEND_SALES', 'SIGN_SALES'], 'Analyst Recommendation', 1)
-    runTests(['RECOMMEND_APPROVAL', 'RECOMMEND_REJECTION', 'REJECTED', 'VALIDATED'], ['RECOMMEND_SALES', 'SIGN_SALES'], 'View the credit application detail as processed by the analyst.', 1)
-  })
+  // describe('renders certain elements based on validation status and user permissions', () => {
+  //   const runTests = (statuses, permissions, element, numberOfOccurences) => {
+  //     for (const status of statuses) {
+  //       for (const permission of permissions) {
+  //         test(`renders "${element}" element when validation status is ${status} and user has permission ${permission}`, () => {
+  //           const submission = {
+  //             validationStatus: status
+  //           }
+  //           const user = {
+  //             hasPermission: getHasPermission(permission)
+  //           }
+  //           const props = { ...baseProps, submission, user }
+  //           const { queryAllByText } = render(
+  //             <Router>
+  //               <AnalystRecommendationHeader
+  //                 {...props}
+  //               />
+  //             </Router>
+  //           )
+  //           expect(queryAllByText('Analyst Recommendation')).toHaveLength(numberOfOccurences)
+  //         })
+  //       }
+  //     }
+  //   }
+  //   runTests(['RECOMMEND_APPROVAL', 'RECOMMEND_REJECTION', 'CHECKED', 'REJECTED', 'VALIDATED'], ['RECOMMEND_SALES', 'SIGN_SALES'], 'Analyst Recommendation', 1)
+  //   runTests(['RECOMMEND_APPROVAL', 'RECOMMEND_REJECTION', 'REJECTED', 'VALIDATED'], ['RECOMMEND_SALES', 'SIGN_SALES'], 'View the credit application detail as processed by the analyst.', 1)
+  // })
 
-  describe('disables checkbox based on validation status and user permissions', () => {
-    const statuses = ['RECOMMEND_APPROVAL', 'RECOMMEND_REJECTION', 'REJECTED', 'VALIDATED']
-    const permissions = ['SIGN_SALES']
-    for (const status of statuses) {
-      for (const permission of permissions) {
-        test(`analyst checkbox is disabled when validation status is ${status} and user has permission ${permission}`, () => {
-          const submission = {
-            validationStatus: status
-          }
-          const user = {
-            hasPermission: getHasPermission(permission)
-          }
-          const props = { ...baseProps, submission, user }
-          const { getByTestId } = render(
-            <Router>
-              <AnalystRecommendationHeader
-                {...props}
-              />
-            </Router>
-          )
-          expect(getByTestId('analyst-checkbox')).toHaveProperty('disabled', true)
-        })
-      }
-    }
-  })
+  // describe('disables checkbox based on validation status and user permissions', () => {
+  //   const statuses = ['RECOMMEND_APPROVAL', 'RECOMMEND_REJECTION', 'REJECTED', 'VALIDATED']
+  //   const permissions = ['SIGN_SALES']
+  //   for (const status of statuses) {
+  //     for (const permission of permissions) {
+  //       test(`analyst checkbox is disabled when validation status is ${status} and user has permission ${permission}`, () => {
+  //         const submission = {
+  //           validationStatus: status
+  //         }
+  //         const user = {
+  //           hasPermission: getHasPermission(permission)
+  //         }
+  //         const props = { ...baseProps, submission, user }
+  //         const { getByTestId } = render(
+  //           <Router>
+  //             <AnalystRecommendationHeader
+  //               {...props}
+  //             />
+  //           </Router>
+  //         )
+  //         expect(getByTestId('analyst-checkbox')).toHaveProperty('disabled', true)
+  //       })
+  //     }
+  //   }
+  // })
 })
