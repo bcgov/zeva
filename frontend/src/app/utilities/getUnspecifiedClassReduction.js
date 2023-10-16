1325868cb4c8c949b12059019fc8dad60ce42674
julianforeman <julianforeman@me.com>
2023-10-16 11:46:36 -0700

    Moving code to new release

--- frontend/src/app/utilities/getUnspecifiedClassReduction.js
+++ frontend/src/app/utilities/getUnspecifiedClassReduction.js
@@ -1,8 +1,8 @@
-const getUnspecifiedClassReduction = (_totalReduction, _classAReduction) => {
+const getUnspecifiedClassReduction = (_totalReduction, _classAReduction, supplierClass) => {
   const totalReduction = String(_totalReduction).replace(/,/g, '')
   const classAReduction = String(_classAReduction).replace(/,/g, '')
 
-  if (isNaN(totalReduction) || isNaN(classAReduction)) {
+  if (isNaN(totalReduction) || isNaN(classAReduction) || supplierClass === 'S') {
     return 0
   }
 
