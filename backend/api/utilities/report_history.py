e17104f2d23a532ba4aeb1c3d1d0fa61358aeab7
tim738745 <98717409+tim738745@users.noreply.github.com>
2023-10-18 16:00:52 -0700

    fix: zeva-1902 - v5 (#2001)

--- backend/api/utilities/report_history.py
+++ backend/api/utilities/report_history.py
@@ -33,7 +33,7 @@ def exclude_from_history(history, user):
         ).values_list("username")
         result = result.exclude(
             validation_status__in=[
-                ModelYearReportStatuses.SUBMITTED,
+                ModelYearReportStatuses.DRAFT, ModelYearReportStatuses.SUBMITTED,
             ],
             create_user__in=users,
         )
