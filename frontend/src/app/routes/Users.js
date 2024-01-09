dde151885556b9f69bb8233e9339c0178de619e4
julianforeman <julianforeman@me.com>
2024-01-09 09:35:53 -0800

    Added functionality to download email list with organizations attached for filtering

--- frontend/src/app/routes/Users.js
+++ frontend/src/app/routes/Users.js
@@ -5,7 +5,8 @@ const USERS = {
   ME: `${API_BASE_PATH}/current`,
   DETAILS: `${API_BASE_PATH}/:id`,
   EDIT: `${API_BASE_PATH}/:id/edit`,
-  ACTIVE: `${API_BASE_PATH}/active`
+  ACTIVE: `${API_BASE_PATH}/active`,
+  DOWNLOAD_ACTIVE: `${API_BASE_PATH}/download_active`
 }
 
 export default USERS
--- frontend/src/users/components/ActiveUsersListPage.js
+++ frontend/src/users/components/ActiveUsersListPage.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Loading from '../../app/components/Loading'
+import axios from 'axios'
+import ROUTES_USERS from '../../app/routes/Users'
+import Button from '../../app/components/Button'
 
 const ActiveUsersListPage = (props) => {
   const { activeIdirUsers, activeBceidUsers, loading } = props
+
+  const handleDownload = async () => {
+    try {
+      const response = await axios.get(ROUTES_USERS.DOWNLOAD_ACTIVE, {responseType: 'blob'})
+
+      const url = window.URL.createObjectURL(new Blob([response.data]))
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'Active_BCEID_Users.xls'); // File name for download
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    } catch (error) {
+      console.error('Error downloading the file: ', error)
+    }
+    
+  }
 
   if (loading) {
     return <Loading />
@@ -14,7 +34,7 @@ const ActiveUsersListPage = (props) => {
       <div className="col-md-8">
         <h2>Active Users</h2>
       </div>
-      <div className="mt-3 pt-3">
+      <div className="mt-3 mb-3 pt-3">
         <h4>BCEID Users</h4>
         <textarea
           rows="11"
@@ -23,6 +43,14 @@ const ActiveUsersListPage = (props) => {
           readOnly
         />
       </div>
+      <Button
+        buttonType="download"
+        optionalText="Download List"
+        optionalClassname="button primary"
+        action={() => {
+          handleDownload()
+        }}
+      />
       <div className="mt-3 pt-4">
         <h4>IDIR Users</h4>
         <textarea
