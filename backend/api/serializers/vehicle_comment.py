d897703c4f2af6ef7e7f4029dcbbaf465763d0ad
Emily <44536222+emi-hi@users.noreply.github.com>
2024-08-21 13:26:52 -0700

    feat: removes idir names from sales submission and vehicle comments (#2217)

--- backend/api/serializers/vehicle_comment.py
+++ backend/api/serializers/vehicle_comment.py
@@ -15,12 +15,19 @@ class VehicleCommentSerializer(ModelSerializer):
     create_user = SerializerMethodField()
 
     def get_create_user(self, obj):
-        user = UserProfile.objects.filter(username=obj.create_user).first()
-        if user is None:
+        request = self.context.get('request')
+        commenting_user = UserProfile.objects.filter(username=obj.create_user).first()
+        if commenting_user is None:
             return obj.create_user
-
-        serializer = MemberSerializer(user, read_only=True)
-        return serializer.data
+        if not commenting_user.is_government or request.user.is_government:
+            ## if the commentor is not government or the request
+            #  user is government, show all the data
+            serializer = MemberSerializer(commenting_user, read_only=True)
+            return serializer.data
+        else:
+            #if the request user is not government and the commenter
+            #is government
+            return {'display_name': 'Government User'}
 
     class Meta:
         model = VehicleComment
