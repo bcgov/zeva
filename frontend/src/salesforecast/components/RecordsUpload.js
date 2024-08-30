83c421c3cf77b70933c3c64d6dc2bf105effdfcf
tim738745 <98717409+tim738745@users.noreply.github.com>
2024-08-30 10:06:27 -0700

    feat: 2219 - forecast report column mapping + automatic summation of ZEVs supplied (#2231)
    * feat: 2219 - forecast report colum mapping + automatic summation of ZEVs supplied
    
    * small change
    
    * fix totals display upon discard of records

--- frontend/src/salesforecast/components/RecordsUpload.js
+++ frontend/src/salesforecast/components/RecordsUpload.js
@@ -5,47 +5,101 @@ import Button from "../../app/components/Button";
 import FileDropArea from "../../app/components/FileDropArea";
 import FORECAST_ROUTES from "../constants/routes";
 import download from "../../app/utilities/download";
+import columnMapping from "../constants/columnMapping";
 
-const RecordsUpload = ({ setRecords }) => {
+const RecordsUpload = ({ currentModelYear, setRecords, setTotals }) => {
   const [files, setFiles] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
   const getInternalRecords = (records) => {
-    //todo: this function should return a new array L of objects such that any object x in L
-    //is a record with "internal keys" instead of "spreadsheet keys" (e.g. "modelYear" instead of "Model Year").
-    return records;
+    const result = [];
+    for (const record of records) {
+      const internalRecord = {};
+      for (const [spreadsheetName, internalName] of Object.entries(
+        columnMapping,
+      )) {
+        internalRecord[internalName] = record[spreadsheetName];
+      }
+      result.push(internalRecord);
+    }
+    return result;
   };
 
   const validateRecords = (records) => {
-
     const ZEV_TYPE = ["BEV", "PHEV", "FCEV", "EREV"];
-  
-    records.forEach(record => {
+
+    records.forEach((record) => {
       if (!/^\d{4}$/.test(record.modelYear)) {
-        throw new Error("Model year must be a 4-digit string.");
+        throw new Error("Model year must be a 4-digit integer.");
       }
-      if (typeof record.make !== 'string' || record.make.length > 250) {
-        throw new Error("Make must be a string that is less than 250 characters.");
+      if (
+        typeof record.make !== "string" ||
+        record.make === "" ||
+        record.make.length > 250
+      ) {
+        throw new Error(
+          "Make must be a non-empty string that is no more than 250 characters.",
+        );
       }
-      if (typeof record.modelName !== 'string' || record.modelName.length > 250) {
-        throw new Error("Model name must be a string.");
+      if (
+        typeof record.modelName !== "string" ||
+        record.modelName === "" ||
+        record.modelName.length > 250
+      ) {
+        throw new Error(
+          "Model must be a non-empty string that is no more than 250 characters.",
+        );
       }
       if (!ZEV_TYPE.includes(record.type)) {
-        throw new Error(`Type must be one of the following values: ${ZEV_TYPE.join(", ")}.`);
+        throw new Error(
+          `Type must be one of the following values: ${ZEV_TYPE.join(", ")}.`,
+        );
       }
-      if (typeof record.range !== 'number' || !/^\d+(\.\d{1,2})?$/.test(record.range.toString())) {
-        throw new Error("Range must be a decimal number with up to 2 decimal places.");
+      if (
+        typeof record.range !== "number" ||
+        !/^\d+(\.\d{1,2})?$/.test(record.range.toString())
+      ) {
+        throw new Error(
+          "Range must be a number with no more than 2 decimal places.",
+        );
       }
-      if (typeof record.zevClass !== 'string' || record.zevClass.length !== 1) {
+      if (typeof record.zevClass !== "string" || record.zevClass.length !== 1) {
         throw new Error("ZEV class must be a single character.");
       }
-      if (typeof record.vehicleClassInteriorVolume !== 'string' || record.vehicleClassInteriorVolume.length > 250) {
-        throw new Error("Vehicle class interior volume must be a string.");
+      if (
+        typeof record.vehicleClassInteriorVolume !== "string" ||
+        record.vehicleClassInteriorVolume === "" ||
+        record.vehicleClassInteriorVolume.length > 250
+      ) {
+        throw new Error(
+          "Vehicle class and Interior Volume must be a non-empty string that is no more than 250 characters.",
+        );
       }
       if (!Number.isInteger(record.totalSupplied)) {
-        throw new Error("Total supplied must be an integer.");
+        throw new Error("ZEVs Supply Forecast must be an integer.");
       }
     });
+  };
+
+  const getZevTotals = (records) => {
+    const modelYear1 = currentModelYear + 1;
+    const modelYear2 = currentModelYear + 2;
+    const modelYear3 = currentModelYear + 3;
+    let zevVehiclesOne = 0;
+    let zevVehiclesTwo = 0;
+    let zevVehiclesThree = 0;
+    for (const record of records) {
+      const modelYear = record.modelYear;
+      const totalSupplied = record.totalSupplied;
+      if (modelYear === modelYear1) {
+        zevVehiclesOne = zevVehiclesOne + totalSupplied;
+      } else if (modelYear === modelYear2) {
+        zevVehiclesTwo = zevVehiclesTwo + totalSupplied;
+      } else if (modelYear === modelYear3) {
+        zevVehiclesThree = zevVehiclesThree + totalSupplied;
+      }
+    }
+    return { zevVehiclesOne, zevVehiclesTwo, zevVehiclesThree };
   };
 
   useEffect(() => {
@@ -68,6 +122,10 @@ const RecordsUpload = ({ setRecords }) => {
               const internalRecords = getInternalRecords(records);
               validateRecords(internalRecords);
               setRecords(internalRecords);
+              const zevTotals = getZevTotals(internalRecords);
+              setTotals((prev) => {
+                return { ...prev, ...zevTotals };
+              });
             } catch (error) {
               setErrorMessage(error.message);
             }
