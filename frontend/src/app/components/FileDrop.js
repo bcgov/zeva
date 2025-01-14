06c775514c08a959edb27c8488f639e752e026b8
Jonny Toombs <jdtoombs@gmail.com>
2025-01-14 10:46:10 -0800

    ZEVA-2290: Forecast upload notification improvement (#2418)
    * enhance ux on file upload
    
    * allow for prescence of confirm modal to be controlled via props for FileDropArea

--- frontend/src/app/components/FileDrop.js
+++ frontend/src/app/components/FileDrop.js
@@ -1,58 +1,63 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import PropTypes from 'prop-types'
-import React, { useCallback, useState } from 'react'
-import { useDropzone } from 'react-dropzone'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 const FileDrop = ({
   setErrorMessage,
   setFiles,
   maxFiles,
-  allowedFileTypes
+  allowedFileTypes,
+  currFileLength,
 }) => {
-  const [dropMessage, setDropMessage] = useState('')
+  const [dropMessage, setDropMessage] = useState("");
   const onDrop = useCallback((acceptedFiles) => {
     if (acceptedFiles.length > maxFiles) {
       setDropMessage(
-        `Please select only ${maxFiles} file${maxFiles !== 1 ? 's' : ''}.`
-      )
+        `Please select only ${maxFiles} file${maxFiles !== 1 ? "s" : ""}.`,
+      );
     } else {
-      setDropMessage('')
-      setErrorMessage('')
-      setFiles(acceptedFiles)
+      setDropMessage("");
+      setErrorMessage("");
+      setFiles(acceptedFiles);
     }
-  }, [])
+  }, []);
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    accept: allowedFileTypes
-  })
+    accept: allowedFileTypes,
+  });
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
 
-      <div className="file-upload">
+      <div className={!currFileLength ? "file-upload" : "file-upload disabled"}>
         <FontAwesomeIcon icon="upload" />
         <br />
         Drag and Drop files here or <br />
-        <button className="link text-center" type="button">
-          browse to select a file from your machine to upload.
+        <button
+          disabled={!!currFileLength}
+          className={!currFileLength ? "link text-center" : "link-disabled"}
+          type="button"
+        >
+          Browse to select a file from your machine to upload.
         </button>
         {dropMessage && <div id="danger-text">{dropMessage}</div>}
       </div>
     </div>
-  )
-}
+  );
+};
 
 FileDrop.defaultProps = {
   setErrorMessage: () => {},
-  allowedFileTypes: null
-}
+  allowedFileTypes: null,
+};
 
 FileDrop.propTypes = {
   setErrorMessage: PropTypes.func,
   setFiles: PropTypes.func.isRequired,
   maxFiles: PropTypes.number.isRequired,
-  allowedFileTypes: PropTypes.string
-}
+  allowedFileTypes: PropTypes.string,
+};
 
-export default FileDrop
+export default FileDrop;
