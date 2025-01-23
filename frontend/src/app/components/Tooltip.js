da3d94d487456ed1f3d02dd17f1282bdc36557b0
emi-hi <emily.hillier@gmail.com>
2025-01-23 13:05:39 -0800

    chore: more linting

--- frontend/src/app/components/Tooltip.js
+++ frontend/src/app/components/Tooltip.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactTooltip from 'react-tooltip';
-import PropTypes from 'prop-types';
+import React from "react";
+import ReactTooltip from "react-tooltip";
+import PropTypes from "prop-types";
 
-const Tooltip = ({ tooltipId, tooltipText, children, placement = 'top' }) => (
+const Tooltip = ({ tooltipId, tooltipText, children, placement = "top" }) => (
   <span data-for={tooltipId} data-tip={tooltipText} className="tooltip-wrapper">
     <ReactTooltip
       id={tooltipId}
