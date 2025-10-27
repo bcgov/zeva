eda1a58a2d2aa2eac0abf15169b3100e934045d7
Kuan Fan <kuan.fan@gov.bc.ca>
2025-10-27 14:23:52 -0700

    use podman

--- openshift/scripts/import-data.sh
+++ openshift/scripts/import-data.sh
@@ -3,7 +3,7 @@ set -e
 
 # This script creates a pg_dump .tar file from an openshift pod
 # and automatically downloads and restores the .tar into 
-# a local postgres docker container
+# a local postgres podman container
 
 # 2 Arguments 
 # $1 = 'test' or 'prod'
@@ -18,7 +18,7 @@ fi
 
 # defaults set to dev
 project_name='e52f12-dev'
-pod_name='zeva-crunchy-dev-zeva-spxf-0'
+pod_name='zeva-dev-crunchy-zeva-p4bq-0'
 env='dev'
 
 local_container=$2 
@@ -52,12 +52,12 @@ oc rsync $pod_name:/tmp/zeva.tar ./
 echo
 
 echo "** Copying .tar to local database container $local_container"
-docker cp zeva.tar $local_container:/tmp/zeva.tar
+podman cp zeva.tar $local_container:/tmp/zeva.tar
 echo
 
 echo "** Restoring local database"
-docker exec $local_container bash -c 'pg_restore -U postgres -F t --no-privileges --no-owner -c -d zeva < /tmp/zeva.tar' || true
-docker exec $local_container bash -c 'pg_restore -U postgres -F t --no-privileges --no-owner -c -d zeva < /tmp/zeva.tar'
+podman exec $local_container bash -c 'pg_restore -U postgres -F t --no-privileges --no-owner -c -d zeva < /tmp/zeva.tar' || true
+podman exec $local_container bash -c 'pg_restore -U postgres -F t --no-privileges --no-owner -c -d zeva < /tmp/zeva.tar'
 echo
 
 echo "** Cleaning up"
